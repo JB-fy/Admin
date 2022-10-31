773d39bc59ce72afaeae4cacdc7cb35064d19afe
13599995734 <274456806@qq.com>
2022-10-31 21:14:56 +0800

    1

--- api/app/module/service/AbstractService.php
+++ api/app/module/service/AbstractService.php
@@ -13,36 +13,92 @@ abstract class AbstractService
     //  */
     // protected $table;
     //protected $table = \app\module\db\table\auth\AuthMenu::class;
-    protected $table = '';   //table类的路径，调用地方实例化对象。因table类带有状态，无法直接使用容器注释功能作依赖注入。
+    protected $table = '';   //table类的路径，调用地方实例化对象。因table类带有状态，无法直接使用容器注释功能做依赖注入。
 
     /**
-     * 获取列表（通用，如果某些类需要特殊处理，可重新定义）
+     * 列表
+     * 
+     * @param array $field
+     * @param array $where
+     * @param array $order
+     * @param integer $offset
+     * @param integer $limit
+     * @return void
      */
     public function list(array $field = [], array $where = [], array $order = [], int $offset = 1, int $limit = 10)
     {
         $countAfter = ($offset == 0 && $limit == 0);  //用于判断是否先获取$list，再通过count($list)计算$count
-        $tableAuthMenu = container($this->table, true);
-        $tableAuthMenu->where($where);
+        $table = container($this->table, true);
+        $table->where($where);
         if (!$countAfter) {
-            if ($tableAuthMenu->isJoin()) {
-                $count = $tableAuthMenu->getBuilder()->distinct()->count($tableAuthMenu->getTable() . '.' . $tableAuthMenu->getKey());
+            if ($table->isJoin()) {
+                $count = $table->getBuilder()->distinct()->count($table->getTable() . '.' . $table->getKey());
             } else {
-                $count = $tableAuthMenu->getBuilder()->count();
+                $count = $table->getBuilder()->count();
             }
         }
 
         $list = [];
         if ($countAfter || $count > $offset) {
-            $tableAuthMenu->field($field)->order($order);
-            if ($tableAuthMenu->isJoin()) {
-                $tableAuthMenu->group(['id']);
+            $table->field($field)->order($order);
+            if ($table->isJoin()) {
+                $table->group(['id']);
             }
-            $list = $tableAuthMenu->getList($offset, $limit);
+            $list = $table->getList($offset, $limit);
             if ($countAfter) {
                 $count = count($list);
             }
         }
 
         throwSuccessJson(['count' => $count, 'list' => $list]);
+    }
+
+    /**
+     * 创建
+     *
+     * @param array $data
+     * @return void
+     */
+    public function create(array $data)
+    {
+        $table = container($this->table, true);
+        $id = $table->insert($data)->saveInsert();
+        if (empty($id)) {
+            throwFailJson('999999');
+        }
+        throwSuccessJson();
+    }
+
+    /**
+     * 修改
+     *
+     * @param array $data
+     * @param integer $id
+     * @return void
+     */
+    public function update(array $data, int $id)
+    {
+        $table = container($this->table, true);
+        $result = $table->where(['id' => $id])->update($data)->saveUpdate();
+        if (empty($result)) {
+            throwFailJson('999999');
+        }
+        throwSuccessJson();
+    }
+
+    /**
+     * 删除
+     *
+     * @param array $idArr
+     * @return void
+     */
+    public function delete(array $idArr)
+    {
+        $table = container($this->table, true);
+        $result = $table->where([['id', 'in', $idArr]])->delete();
+        if (empty($result)) {
+            throwFailJson('999999');
+        }
+        throwSuccessJson();
     }
 }
