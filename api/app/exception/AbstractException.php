765d52574af4617f3027d5c2888fdc3bc9006986
13599995734 <274456806@qq.com>
2022-11-05 09:32:03 +0800

    1

--- api/app/exception/AbstractException.php
+++ api/app/exception/AbstractException.php
@@ -6,4 +6,10 @@ namespace app\exception;
 
 abstract class AbstractException extends \Exception
 {
+    /**
+     * 获取response
+     *
+     * @return \support\Response
+     */
+    abstract public function getResponse(): \support\Response;
 }
--- api/app/exception/handler/Handler.php
+++ api/app/exception/handler/Handler.php
@@ -12,10 +12,7 @@ class Handler extends AbstractHandler
 {
     public function render(Request $request, Throwable $e): Response
     {
-        /* if ($e instanceof \app\exception\AbstractException) {
-            return $e->getResponse();
-        } */
-        if ($e instanceof \app\exception\Json) {
+        if ($e instanceof \app\exception\AbstractException) {
             return $e->getResponse();
         } elseif ($e instanceof \think\exception\ValidateException) {
             $responseData = [
@@ -25,8 +22,6 @@ class Handler extends AbstractHandler
             ];
             $responseBody = json_encode($responseData, JSON_UNESCAPED_UNICODE);
             return response($responseBody)->withHeader('Content-Type', 'application/json');
-        } elseif ($e instanceof \app\exception\Raw) {
-            return $e->getResponse();
         }
         return parent::render($request, $e);
     }
