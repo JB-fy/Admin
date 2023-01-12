ed26dfd0e2b0b55db3c351bef34da69bb428dc9a
13599995734 <274456806@qq.com>
2023-01-12 23:47:27 +0800

    1

--- dev/admin/platform/src/views/Index.vue
+++ dev/admin/platform/src/views/Index.vue
@@ -3,7 +3,15 @@ const { t } = useI18n()
 </script>
 
 <template>
-    <div>
-        <div style="text-align: center; font-size: 300px; color: #409EFF;">{{ t('view.index.welcome') }}</div>
+    <div style="text-align: center; font-size: 300px; color: #409EFF;">
+        <ElSpace :size="0">
+            <ElIcon class="is-loading">
+                <AutoiconEpOrange />
+            </ElIcon>
+            {{ t('view.index.welcome') }}
+            <ElIcon class="is-loading">
+                <AutoiconEpOrange />
+            </ElIcon>
+        </ElSpace>
     </div>
 </template>
