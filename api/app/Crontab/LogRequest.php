ce68b6a0cce8b1ac4ffc0a931e5f2cb60976fd30
13599995734 <274456806@qq.com>
2023-01-16 14:30:13 +0800

    1

---
+++ api/app/Crontab/LogRequest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Crontab;
+
+use App\Module\Db\Dao\Log\Request;
+use Hyperf\DbConnection\Db;
+/* use Hyperf\Di\Annotation\Inject;
+use Psr\Container\ContainerInterface;
+
+class OrderTask
+{
+    #[Inject]
+    protected ContainerInterface $container; */
+
+class OrderTask
+{
+    /* 
+    //查看分区
+    SELECT PARTITION_NAME, PARTITION_EXPRESSION, PARTITION_DESCRIPTION, TABLE_ROWS
+    FROM INFORMATION_SCHEMA.PARTITIONS
+    WHERE TABLE_SCHEMA = SCHEMA() AND TABLE_NAME = '表名';
+    //修改表为分区表
+    ALTER TABLE `表名` PARTITION BY RANGE (TO_DAYS( addTime ))
+    (PARTITION `p20190204` VALUES LESS THAN (TO_DAYS('2019-02-05')) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0 ,
+    PARTITION `pMax` VALUES LESS THAN (MAXVALUE) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0 );
+    //新增分区
+    ALTER TABLE `表名` ADD PARTITION
+    (PARTITION `p20190107` VALUES LESS THAN (TO_DAYS('2019-01-08')) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0 ,
+    PARTITION `p20190108` VALUES LESS THAN (TO_DAYS('2019-01-09')) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0 );
+    //删除分区
+    ALTER TABLE `表名` DROP PARTITION p20190107,p20190108;
+    */
+    /**
+     * 请求日志表每周新增分区（无分区时建立分区）
+     */
+    public function partition()
+    {
+        $dao = getDao(Request::class);
+        $connection = $dao->getConnection();
+        $table = $dao->getTable();
+        $db = Db::connection($connection);
+        /**--------查询分区（不是分区表或无分区，查询结果都会有一项，且第一项内PARTITION_NAME值为null） 开始--------**/
+        $partitionSelSql = 'SELECT PARTITION_NAME FROM INFORMATION_SCHEMA.PARTITIONS WHERE TABLE_SCHEMA = SCHEMA() AND TABLE_NAME = \'' . $table . '\'';
+        $partitionResult = $db->select($partitionSelSql, [], false);
+        var_dump($partitionResult);
+        var_dump($db->selectOne($partitionSelSql, [], false));
+        return;
+        $partitionList = [];
+        if ($partitionResult[0]['PARTITION_NAME'] !== null) {
+            foreach ($partitionResult as $v) {
+                $partitionList[] = $v['PARTITION_NAME'];
+            }
+        }
+        /**--------查询分区（不是分区表或无分区，查询结果都会有一项，且第一项内PARTITION_NAME值为null） 结束--------**/
+
+        /**--------无分区则建立当天的分区 开始--------**/
+        if (empty($partitionList)) {
+            $partitionCreateSql = 'PARTITION `p' . date('Ymd') . '` VALUES LESS THAN (TO_DAYS( \'' . date('Y-m-d', time() + 24 * 60 * 60) . '\' )) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0';
+            $partitionCreateSql = 'ALTER TABLE `' . $table . '` PARTITION BY RANGE (TO_DAYS( addTime )) ( ' . $partitionCreateSql . ' )';
+            $db->update($partitionCreateSql);
+        }
+        /**--------无分区则建立当天的分区 结束--------**/
+
+        /**--------检测后面7天的分区是否存在，没有则新增分区 开始--------**/
+        $partitionAddSql = '';
+        for ($i = 1; $i < 7; $i++) {
+            if (!in_array('p' . date('Ymd', time() + $i * 24 * 60 * 60), $partitionList)) {
+                $partitionAddSql .= 'PARTITION `p' . date('Ymd', time() + $i * 24 * 60 * 60) . '` VALUES LESS THAN (TO_DAYS( \'' . date('Y-m-d', time() + ($i + 1) * 24 * 60 * 60) . '\' )) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0 ,';
+            }
+        }
+        if (!in_array('p' . date('Ymd', time() + $i * 24 * 60 * 60), $partitionList)) {
+            $partitionAddSql .= 'PARTITION `p' . date('Ymd', time() + $i * 24 * 60 * 60) . '` VALUES LESS THAN (TO_DAYS( \'' . date('Y-m-d', time() + ($i + 1) * 24 * 60 * 60) . '\' )) ENGINE = InnoDB MAX_ROWS = 0 MIN_ROWS = 0';
+        }
+        if ($partitionAddSql) {
+            $partitionAddSql = 'ALTER TABLE `' . $table . '` ADD PARTITION ( ' . $partitionAddSql . ' )';
+            $db->update($partitionAddSql);
+        }
+        /**--------检测后面7天的分区是否存在，没有则新增分区 结束--------**/
+    }
+}
---
+++ api/config/autoload/crontab.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use Hyperf\Crontab\Crontab;
+
+/**
+ *  定时器说明
+ *  * * * * * *
+ *  | | | | | |--星期（0-6，星期天为0）
+ *  | | | | |----月（1-12）
+ *  | | | |------日（1-31）
+ *  | | |--------时（0-23）
+ *  | |----------分（0-59）
+ *  |------------秒（0-59）
+ */
+return [
+    'enable' => env('ENABLE_CRONTAB', false),//是否开启定时任务
+    'crontab' => [//通过配置文件定义的定时任务
+        /**--------Callback类型定时任务（默认） 开始--------**/
+        (new Crontab())
+            ->setName('LogRequestPartition')
+            ->setCallback([App\Crontab\LogRequest::class, 'partition'])
+            ->setRule('0 */15 3 * * 0')//星期天的凌晨3点每15分钟执行一次（多次执行防止新增分区失败）
+            //->setRule('*/5 * * * * *')
+            //->setSingleton(true)
+            ->setMemo('请求日志表每周新增分区'),
+        /**--------Callback类型定时任务（默认）结束--------**/
+
+        /**--------Command类型定时任务 开始--------**/
+        /*(new Crontab())
+            ->setType('command')
+            ->setName('Bar')
+            ->setRule('* * * * *')
+            ->setCallback([
+                'command' => 'swiftmailer:spool:send',
+                // (optional) arguments
+                'fooArgument' => 'barValue',
+                // (optional) options
+                '--message-limit' => 1,
+            ]),*/
+        /**--------Command类型定时任务 结束--------**/
+    ],
+];
