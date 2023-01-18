62e364f46941dac9fd6bdc73ae44b806e39f5e4f
13599995734 <274456806@qq.com>
2023-01-18 09:39:54 +0800

    1

--- api/app/Controller/Upload.php
+++ api/app/Controller/Upload.php
@@ -12,20 +12,23 @@ class Upload extends AbstractController
      */
     public function sign()
     {
+        $option = [];
+        $type = $this->request->input('type');
+        switch ($type) {
+            case 'type1':
+                /* $option = [
+                    'callbackEnable' => true, //是否回调服务器
+                    'expireTime' => 5 * 60, //签名有效时间
+                    'dir' => 'common/' . date('Y/m/d/His') . mt_rand(1000, 9999) . '_',    //上传的文件前缀
+                    'minSize' => 0,    //限制上传的文件大小。单位：字节
+                    'maxSize' => 100 * 1024 * 1024,    //限制上传的文件大小。单位：字节
+                ]; */
+                break;
+        }
         /**
          * @var \App\Plugin\Upload\AbstractUpload
          */
         $upload = $this->container->get('upload');
-
-        $option = [];
-        //$data = $this->request->all();
-        /* $option = [
-            'isCallback' => true, //是否回调服务器
-            'expireTime' => 5 * 60, //签名有效时间
-            'dir' => 'common/' . date('Y/m/d/His') . mt_rand(1000, 9999) . '_',    //上传的文件前缀
-            'minSize' => 0,    //限制上传的文件大小。单位：字节
-            'maxSize' => 100 * 1024 * 1024,    //限制上传的文件大小。单位：字节
-        ]; */
         $upload->createSign($option);
     }
 
--- api/config/autoload/crontab.php
+++ api/config/autoload/crontab.php
@@ -15,7 +15,7 @@ use Hyperf\Crontab\Crontab;
  *  |------------秒（0-59）
  */
 return [
-    'enable' => env('ENABLE_CRONTAB', false), //是否开启定时任务
+    'enable' => env('CRONTAB_ENABLE', true), //是否开启定时任务
     //通过配置文件定义的定时任务
     'crontab' => [
         /**--------Callback类型定时任务（默认） 开始--------**/
