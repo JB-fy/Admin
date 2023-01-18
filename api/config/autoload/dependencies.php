df28e51b76cc730c6820b863d48e2a61450b8746
13599995734 <274456806@qq.com>
2023-01-18 21:19:45 +0800

    1

--- api/config/autoload/dependencies.php
+++ api/config/autoload/dependencies.php
@@ -13,20 +13,6 @@ use Psr\Container\ContainerInterface;
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    //上传组件
-    'upload' => function (ContainerInterface $container) {
-        /* $config = [
-            'accessId' => 'LTAI5tHx81H64BRJA971DPZF',
-            'accessKey' => 'nJyNpTtUuIgZqx21FF4G2zi0WHOn51',
-            'host' => 'http://4724382110.oss-cn-hongkong.aliyuncs.com'
-        ]; */
-        $config = [
-            'accessId' => 'LTAI5tSjYikt3bX33riHezmk',
-            'accessKey' => 'k4uRZU6flv73yz1j4LJu9VY5eNlHas',
-            'host' => 'https://gamemt.oss-cn-hangzhou.aliyuncs.com'
-        ];
-        return make(\App\Plugin\Upload\AliyunOss::class, ['config' => $config]);
-    },
     //全部场景列表（即表auth_scene数据）
     'allSceneList' => function (ContainerInterface $container) {
         $allSceneList = getDao(App\Module\Db\Dao\Auth\Scene::class)->getList();
@@ -42,5 +28,19 @@ return [
         $config = json_decode($config, true); */
         $sceneInfo = getContainer()->get(\App\Module\Logic\Auth\Scene::class)->getInfo('platformAdmin');
         return make(\App\Plugin\Jwt::class, ['config' => $sceneInfo->sceneConfig]);
-    }
+    },
+    //上传组件
+    'upload' => function (ContainerInterface $container) {
+        /* $config = [
+            'accessId' => 'LTAI5tHx81H64BRJA971DPZF',
+            'accessKey' => 'nJyNpTtUuIgZqx21FF4G2zi0WHOn51',
+            'host' => 'http://4724382110.oss-cn-hongkong.aliyuncs.com'
+        ]; */
+        $config = [
+            'accessId' => 'LTAI5tSjYikt3bX33riHezmk',
+            'accessKey' => 'k4uRZU6flv73yz1j4LJu9VY5eNlHas',
+            'host' => 'https://gamemt.oss-cn-hangzhou.aliyuncs.com'
+        ];
+        return make(\App\Plugin\Upload\AliyunOss::class, ['config' => $config]);
+    },
 ];
