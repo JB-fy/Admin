730fd6bfa3d821bd19cd546a4021ca997d933e0a
13599995734 <274456806@qq.com>
2023-02-03 16:45:46 +0800

    1

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
 <?php
 
+var_dump(openssl_encrypt('123456', 'AES-128-ECB', 'e26e543ce222cc0f', 0));
+var_dump(openssl_decrypt('dasfsdfds', 'AES-128-ECB', 'e26e543ce222cc0f', 0));
 var_dump(strlen('哈哈'));
