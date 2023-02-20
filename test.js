5f77b3f612fba5c55873afa6d3144a8e0916e96a
13599995734 <274456806@qq.com>
2023-02-20 10:43:05 +0800

    1

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 
+console.log('哈哈'.length)
+
 event.preventDefault(); //阻止冒泡：标签的默认动作
 event.stopPropagation();    //阻止冒泡：标签的click事件
-console.log('哈哈'.length)
+
+window.onscroll = function () {
+    let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    if (scrollTop + $(window).height() >= $(document).height() - 50) {
+        //滚动到距离底部50px时操作
+    }
+}
