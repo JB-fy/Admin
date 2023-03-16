7a40766a842b41579a34643674e650eb0a8ed68a
13599995734 <274456806@qq.com>
2023-03-16 10:26:29 +0800

    1

--- 示例.go
+++ 示例.go
@@ -5,3 +5,14 @@ import "fmt"
 func main() {
    fmt.Println("Hello, World!")
 }
+
+//打印结构体
+fmt.Printf("%#v", pageInfo)
+
+//get参数获取
+page := c.QueryMap("page")
+//post参数获取（Content-Type: application/x-www-form-urlencoded）
+pageSize := c.PostFormMap("pageSize")
+//post参数获取（Content-Type: application/json）
+// var pageInfo systemReq.SearchApiParams
+// err := c.ShouldBindJSON(&pageInfo)
