7e383c3a02583cb0d2954fdeffd72b0c176a6ea4
13599995734 <274456806@qq.com>
2023-03-16 15:19:36 +0800

    1

--- 示例.go
+++ 示例.go
@@ -1,18 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main() {
-   fmt.Println("Hello, World!")
+	"github.com/gin-gonic/gin"
+)
+
+type SearchApiParams struct {
+	OrderKey string `json:"orderKey"` // 排序
+	Desc     bool   `json:"desc"`     // 排序方式:升序false(默认)|降序true
 }
 
-//打印结构体
-fmt.Printf("%#v", pageInfo)
+func main() {
+	fmt.Println("Hello, World!")
+	//打印结构体
+	var pageInfo SearchApiParams
+	fmt.Printf("%#v", pageInfo)
 
-//get参数获取
-page := c.QueryMap("page")
-//post参数获取（Content-Type: application/x-www-form-urlencoded）
-pageSize := c.PostFormMap("pageSize")
-//post参数获取（Content-Type: application/json）
-// var pageInfo systemReq.SearchApiParams
-// err := c.ShouldBindJSON(&pageInfo)
+	/* gin框架使用方法 */
+	c := *gin.Context
+	//path参数获取（/user/:page/*action"）
+	page := c.Param("page")
+	action := c.Param("action")
+	//get参数获取
+	page := c.Query("page")
+	//post参数获取，对应application/x-www-form-urlencoded和from-data格式参数
+	page := c.PostForm("page")
+	//page := c.PostFormMap("page")
+	//post参数获取（Content-Type: application/json）
+	// var pageInfo systemReq.SearchApiParams
+	// err := c.ShouldBindJSON(&pageInfo)
+}
