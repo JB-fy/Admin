3610460320e238a4237256a7f1f5c967a94368b5
13599995734 <274456806@qq.com>
2023-05-07 14:04:23 +0800

    1

--- api/internal/controller/hello/hello.go
+++ api/internal/controller/hello/hello.go
@@ -2,8 +2,10 @@ package hello
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
 
 	v1 "api/api/hello/v1"
 )
@@ -15,6 +17,12 @@ func New() *Controller {
 }
 
 func (c *Controller) Hello(ctx context.Context, req *v1.Req) (res *v1.Res, err error) {
+	fmt.Println(ctx)
 	g.RequestFromCtx(ctx).Response.Writeln("Hello World!")
 	return
+}
+
+func (c *Controller) Test(r *ghttp.Request) {
+	fmt.Println(r.GetCtx())
+	r.Response.Writeln("Test")
 }
