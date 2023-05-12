1dc23cedcba2b9401456ad9f66e6bb667cc03d47
13599995734 <274456806@qq.com>
2023-05-12 11:31:43 +0800

    1

--- api/internal/controller/hello/hello.go
+++ api/internal/controller/hello/hello.go
@@ -2,12 +2,12 @@ package hello
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 
 	v1 "api/api/hello/v1"
+	dao "api/internal/dao/auth"
 )
 
 type Controller struct{}
@@ -17,12 +17,13 @@ func New() *Controller {
 }
 
 func (c *Controller) Hello(ctx context.Context, req *v1.Req) (res *v1.Res, err error) {
-	fmt.Println(ctx)
+	//fmt.Println(ctx)
+	dao.Menu.Key()
 	g.RequestFromCtx(ctx).Response.Writeln("Hello World!")
 	return
 }
 
 func (c *Controller) Test(r *ghttp.Request) {
-	fmt.Println(r.GetCtx())
+	//fmt.Println(r.GetCtx())
 	r.Response.Writeln("Test")
 }
