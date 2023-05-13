9da003622fbe23ee149f0195ee818361b82dd898
13599995734 <274456806@qq.com>
2023-05-13 09:15:05 +0800

    1

--- dev/admin/platform/vite.config.ts
+++ dev/admin/platform/vite.config.ts
@@ -9,20 +9,16 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  /* if (command === 'serve') {
-    // dev 独有配置
+  /* if (command === 'serve') {  //dev独有配置
     return {}
-  } else {
-    // command === 'build'
-    // build 独有配置
-    return {
-    }
+  } else if (command === 'build') {//build独有配置
+    return {}
   } */
   const env = loadEnv(mode, process.cwd(), ''); //设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
   return {
-    // define: {
-    //   'process.env': env
-    // },
+    /* define: {
+      'process.env': env
+    }, */
     base: env.VITE_BASE_PATH, //URL部署时的目录。打包时给所有静态资源路径加上该前缀,
     build: {
       outDir: '../../..' + env.VITE_BASE_PATH, //构建文件的输出目录
@@ -72,7 +68,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
           ElementPlusResolver(),  //ElementPlus
           VantResolver(), //Vant
           IconsResolver({ //图标（格式：前缀-集合名-图标名。例：<i-ep-lock />）
-            prefix: 'Autoicon',  //标签前缀。默认前缀"i"，false取消前缀。（一定要设置且是唯一字符串，即除了图标用到，代码中其他地方不能以字符串开头。否则容易冲突报错。例：自定义组件right-header被认定为图标ri/ght-header；false被认定为图标fa/lse。）
+            prefix: 'Autoicon',  //标签前缀。默认前缀"i"，false取消前缀。（必须设置且是唯一字符串。代码中除图标使用外，其他地方不能以该字符串开头，否则会被当作图标处理。比如该值未设置，自定义组件rightHeader、布尔值false等会被当作图标处理）
             /* enabledCollections: ['ep'], //启用哪个图标集合，默认启用全部。全部可选集合：https://icones.js.org/
             alias: { //一些复杂的集合名称设置别名
               //别名: '集合名',
@@ -86,9 +82,9 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
 
       /*--------按需导入组件 开始--------*/
       Components({
-        // dirs: [
-        //   'src/app/components',
-        // ],
+        /* dirs: [
+          'src/app/components',
+        ], */
         resolvers: [
           ElementPlusResolver(),
           VantResolver(),
