427132d519188a6572802f1d1a1f3893e12cf5b1
13599995734 <274456806@qq.com>
2023-05-25 18:13:50 +0800

    1

--- api/internal/controller/hello/hello.go
+++ api/internal/controller/hello/hello.go
@@ -25,7 +25,8 @@ func (c *Controller) Hello(ctx context.Context, req *v1.Req) (res *v1.Res, err e
 
 func (c *Controller) Test(r *ghttp.Request) {
 	daoLog.Request.Ctx(r.GetCtx()).Where("logId", 6).OrderAsc("logId").OrderAsc("createTime").All()
-
+	//daoLog.Request.Ctx(r.GetCtx()).Data("runTime", 2, "requestUrl", "1").Insert()
+	//daoLog.Request.Ctx(r.GetCtx()).Data(g.Map{"requestUrl": "1", "runTime": 2}).Insert()
 	joinCode := []string{}
 	daoAuth.Menu.Ctx(r.GetCtx()).Handler(daoAuth.Menu.Filter(g.MapStrAny{"id": 2}, &joinCode)).All()
 	/* res, _ := daoLog.Request.Ctx(r.GetCtx()).Where("logId", 6).All()
