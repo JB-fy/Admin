bf7e7b32542b8105c3d8dabcebbe8c00c7c0bee0
13599995734 <274456806@qq.com>
2023-05-29 23:32:35 +0800

    1

--- api/internal/controller/hello/hello.go
+++ api/internal/controller/hello/hello.go
@@ -30,8 +30,9 @@ func (c *Controller) Test(r *ghttp.Request) {
 	daoLog.Request.Ctx(r.GetCtx()).Fields("logId").Where("logId", 6).Order("logId", "Desc").OrderAsc("createTime").All()
 	//daoLog.Request.Ctx(r.GetCtx()).Data("runTime", 2, "requestUrl", "1").Insert()
 	//daoLog.Request.Ctx(r.GetCtx()).Data(g.Map{"requestUrl": "1", "runTime": 2}).Where("logId", 6).Update()
-	joinCode := []string{}
-	daoAuth.Menu.Ctx(r.GetCtx()).Handler(daoAuth.Menu.ParseField([]string{"id", "createTime"}, &[]string{}, &joinCode), daoAuth.Menu.ParseFilter(g.MapStrAny{"id": 2, "menuId > ?": 22}, &joinCode)).All()
+	joinCodeArr := []string{}
+	afterField := []string{}
+	daoAuth.Menu.Ctx(r.GetCtx()).Handler(daoAuth.Menu.ParseField([]string{"id", "createTime"}, &afterField, &joinCodeArr), daoAuth.Menu.ParseFilter(g.MapStrAny{"id": 2, "menuId > ?": 22}, &joinCodeArr)).All()
 	fmt.Println(daoAuth.Menu.ColumnArr())
 	fmt.Println(daoAuth.Menu.Columns())
 	fmt.Println(gconv.Map(daoAuth.Menu.Columns()))
