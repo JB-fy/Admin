0f888d0207dab418225e01ea8e870d1984dbb586
13599995734 <274456806@qq.com>
2023-06-04 11:27:17 +0800

    1

--- api/internal/service/auth.go
+++ api/internal/service/auth.go
@@ -12,18 +12,6 @@ import (
 )
 
 type (
-	IAction interface {
-		Count(ctx context.Context, filter map[string]interface{}) (count int, err error)
-		List(ctx context.Context, filter map[string]interface{}, field []string, order [2]string, offset int, limit int) (list gdb.Result, err error)
-	}
-	IMenu interface {
-		Count(ctx context.Context, filter map[string]interface{}) (count int, err error)
-		List(ctx context.Context, filter map[string]interface{}, field []string, order [2]string, offset int, limit int) (list gdb.Result, err error)
-	}
-	IRole interface {
-		Count(ctx context.Context, filter map[string]interface{}) (count int, err error)
-		List(ctx context.Context, filter map[string]interface{}, field []string, order [2]string, offset int, limit int) (list gdb.Result, err error)
-	}
 	IScene interface {
 		Count(ctx context.Context, filter map[string]interface{}) (count int, err error)
 		List(ctx context.Context, filter map[string]interface{}, field []string, order [2]string, offset int, limit int) (list gdb.Result, err error)
@@ -31,44 +19,8 @@ type (
 )
 
 var (
-	localScene  IScene
-	localAction IAction
-	localMenu   IMenu
-	localRole   IRole
+	localScene IScene
 )
-
-func Action() IAction {
-	if localAction == nil {
-		panic("implement not found for interface IAction, forgot register?")
-	}
-	return localAction
-}
-
-func RegisterAction(i IAction) {
-	localAction = i
-}
-
-func Menu() IMenu {
-	if localMenu == nil {
-		panic("implement not found for interface IMenu, forgot register?")
-	}
-	return localMenu
-}
-
-func RegisterMenu(i IMenu) {
-	localMenu = i
-}
-
-func Role() IRole {
-	if localRole == nil {
-		panic("implement not found for interface IRole, forgot register?")
-	}
-	return localRole
-}
-
-func RegisterRole(i IRole) {
-	localRole = i
-}
 
 func Scene() IScene {
 	if localScene == nil {
