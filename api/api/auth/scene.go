134ceffd855f68e6c2dd1b49430c7d1d9bc6a4d1
13599995734 <274456806@qq.com>
2023-06-04 11:24:05 +0800

    1

--- api/api/auth/scene.go
+++ api/api/auth/scene.go
@@ -11,8 +11,8 @@ type ReqSceneListFilter struct {
 	api.ReqCommonListFilter
 	// 下面根据自己需求修改
 	IsStop      *uint  `c:"isStop,omitempty" p:"isStop" v:"in:0,1"`
-	SceneId     *uint  `c:"{TplTableNameCamelLowerCase}Id,omitempty" p:"{TplTableNameCamelLowerCase}Id" v:"min:1"`
-	SceneName   string `c:"{TplTableNameCamelLowerCase}Name,omitempty" p:"{TplTableNameCamelLowerCase}Name" v:"length:1,30|regex:^[\\p{L}\\p{M}\\p{N}_-]+$"`
+	SceneId     *uint  `c:"sceneId,omitempty" p:"sceneId" v:"min:1"`
+	SceneName   string `c:"sceneName,omitempty" p:"sceneName" v:"length:1,30|regex:^[\\p{L}\\p{M}\\p{N}_-]+$"`
 	SceneCode   string `c:"sceneCode,omitempty" p:"sceneCode" v:"length:1,30|regex:^[\\p{L}\\p{M}\\p{N}_-]+$"`
 	SceneConfig string `c:"sceneConfig,omitempty" p:"sceneConfig" v:"json"`
 }
