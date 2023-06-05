e05e7ca1a59c318d045cb4f6eec355ed78894a85
13599995734 <274456806@qq.com>
2023-06-05 22:11:46 +0800

    1

--- api/internal/middleware/scene_login_of_platform_admin.go
+++ api/internal/middleware/scene_login_of_platform_admin.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"api/internal/utils"
 	"fmt"
+
+	daoPlatform "api/internal/model/dao/platform"
 
 	"github.com/gogf/gf/v2/net/ghttp"
 )
@@ -9,8 +12,66 @@ import (
 func SceneLoginOfPlatformAdmin(r *ghttp.Request) {
 	sceneCode := r.GetCtxVar("sceneInfo")
 	fmt.Println(sceneCode)
-	/* if !verifyToken(sceneCode) {
+	/**--------验证token 开始--------**/
+	token := r.Header.Get("PlatformAdminToken")
+	if token == "" {
+		r.Response.WriteJson(map[string]interface{}{
+			"code": 39994000,
+			"msg":  "token错误",
+			"data": map[string]interface{}{},
+		})
+		return
+	}
+
+	jwt := utils.NewPlatformAdminJWT()
+	claims, err := jwt.ParseToken(token)
+	if err != nil {
+		r.Response.WriteJson(map[string]interface{}{
+			"code": 39994000,
+			"msg":  err.Error(),
+			"data": map[string]interface{}{},
+		})
+		return
+	}
+	/**--------验证token 结束--------**/
+
+	/**--------选做。限制多地登录，多设备登录等情况下可用（前提必须在登录时做过token缓存） 开始--------**/
+	/* TokenKey := fmt.Sprintf(consts.CacheTokenFormat, sceneCode, claims.Account)
+	checkToken, _ := g.Redis().Get(r.GetCtx(), TokenKey)
+	if checkToken.String() != token {
+		r.Response.WriteJson(map[string]interface{}{
+			"code": 39994002,
+			"msg":  err.Error(),
+			"data": map[string]interface{}{},
+		})
 		return
 	} */
+	/**--------选做。限制多地登录，多设备登录等情况下可用（前提必须在登录时做过token缓存） 结束--------**/
+
+	/**--------获取登录用户信息并验证 开始--------**/
+
+	info, _ := daoPlatform.Admin.Ctx(r.GetCtx()).Handler(daoPlatform.Admin.ParseFilter(map[string]interface{}{"adminId": claims.LoginId}, &[]string{})).One()
+
+	if len(info) == 0 {
+		r.Response.WriteJson(map[string]interface{}{
+			"code": 39994003,
+			"msg":  err.Error(),
+			"data": map[string]interface{}{},
+		})
+		return
+	}
+	if info["isStop"].Int() > 0 {
+		r.Response.WriteJson(map[string]interface{}{
+			"code": 39994004,
+			"msg":  err.Error(),
+			"data": map[string]interface{}{},
+		})
+		return
+	}
+	delete(info, "password")
+	delete(info, "isStop")
+
+	r.SetCtxVar("platformAdminInfo", info) //用户信息保存在协程上下文
+	/**--------获取用户信息并验证 结束--------**/
 	r.Middleware.Next()
 }
