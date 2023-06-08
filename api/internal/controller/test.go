025d7a4f1f09efe81fa545c3a562ca0fe1b164b8
13599995734 <274456806@qq.com>
2023-06-08 10:35:50 +0800

    1

--- api/internal/controller/test.go
+++ api/internal/controller/test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
-	"github.com/gogf/gf/v2/util/grand"
 )
 
 type Test struct{}
@@ -38,29 +37,29 @@ func (c *Test) Test(r *ghttp.Request) {
 	if err != nil {
 		r.Response.Writeln(err.Error())
 		return
-	}
-	fmt.Println(req) */
+	} */
+
+	// fmt.Println(garray.NewStrArrayFrom([]string{"a", "b", "c"}).Contains("a"))
+
+	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Diff(gset.NewIntSetFrom([]int{1, 3})).Slice())
+
+	// fmt.Println(grand.N(1000, 9999))
+	// fmt.Println(grand.Intn(1))
+	// fmt.Println(grand.Str("abcdefg0123456789", 8))
+	// fmt.Println(grand.S(8))       //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+	// fmt.Println(grand.Digits(8))  //0123456789
+	// fmt.Println(grand.Letters(8)) //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+	// fmt.Println(grand.Symbols(8)) //!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+
+	fmt.Println(daoAuth.RoleRelToMenu.ParseDbCtx(r.GetCtx()).Where("roleId", 1).Array("menuId"))
+
+	fmt.Println(g.Cfg().MustGet(r.GetCtx(), "superPlatformAdminId").Int())
+
 	/* r.Response.WriteJson(map[string]interface{}{
 		"code": 0,
 		"msg":  "成功",
 		"data": map[string]interface{}{},
 	}) */
-
-	//fmt.Println(garray.NewStrArrayFrom([]string{"a", "b", "c"}).Contains("a"))
-
-	//fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Diff(gset.NewIntSetFrom([]int{1, 3})).Slice())
-
-	fmt.Println(grand.N(1000, 9999))
-	fmt.Println(grand.Intn(1))
-	fmt.Println(grand.Str("abcdefg0123456789", 8))
-	fmt.Println(grand.S(8))       //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
-	fmt.Println(grand.Digits(8))  //0123456789
-	fmt.Println(grand.Letters(8)) //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-	fmt.Println(grand.Symbols(8)) //!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
-
-	fmt.Println(daoAuth.RoleRelToMenu.ParseDbCtx(r.GetCtx()).Where("roleId", 1).Array("menuId"))
-
-	fmt.Println(g.Cfg().MustGet(r.GetCtx(), "superPlatformAdminId").Int())
 
 	utils.HttpSuccessJson(r, map[string]interface{}{
 		"list": []map[string]interface{}{},
