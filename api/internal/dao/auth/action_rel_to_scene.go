a67f73a7e1ba53e4c2ef7e3c40315b665ebd5176
13599995734 <274456806@qq.com>
2023-06-24 21:02:17 +0800

    1

--- api/internal/dao/auth/action_rel_to_scene.go
+++ api/internal/dao/auth/action_rel_to_scene.go
@@ -7,9 +7,13 @@ package dao
 import (
 	"api/internal/dao/auth/internal"
 	"context"
+	"strings"
 
+	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 // internalActionRelToSceneDao is internal type for wrapping internal DAO implements.
@@ -53,6 +57,243 @@ func (daoThis *actionRelToSceneDao) ParseDbCtx(ctx context.Context, dbSelDataLis
 		return g.DB(daoThis.ParseDbGroup(dbSelDataList[0])).Model(daoThis.ParseDbTable(dbSelDataList[1])).Safe().Ctx(ctx)
 	default:
 		return daoThis.Ctx(ctx)
+	}
+}
+
+// 解析insert
+func (daoThis *actionRelToSceneDao) ParseInsert(insert []map[string]interface{}, fill ...bool) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		insertData := make([]map[string]interface{}, len(insert))
+		for index, item := range insert {
+			insertData[index] = map[string]interface{}{}
+			for k, v := range item {
+				switch k {
+				case `id`:
+					insertData[index][daoThis.PrimaryKey()] = v
+				default:
+					//数据库不存在的字段过滤掉，未传值默认true
+					if (len(fill) == 0 || fill[0]) && !daoThis.ColumnArrG().Contains(k) {
+						continue
+					}
+					insertData[index][k] = v
+				}
+			}
+		}
+		if len(insertData) == 1 {
+			m = m.Data(insertData[0])
+		} else {
+			m = m.Data(insertData)
+		}
+		return m
+	}
+}
+
+// 解析update
+func (daoThis *actionRelToSceneDao) ParseUpdate(update map[string]interface{}, fill ...bool) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		updateData := map[string]interface{}{}
+		for k, v := range update {
+			switch k {
+			case `id`:
+				updateData[daoThis.Table()+`.`+daoThis.PrimaryKey()] = v
+			default:
+				//数据库不存在的字段过滤掉，未传值默认true
+				if (len(fill) == 0 || fill[0]) && !daoThis.ColumnArrG().Contains(k) {
+					continue
+				}
+				updateData[daoThis.Table()+`.`+k] = gvar.New(v) //因下面bug处理方式，json类型字段传参必须是gvar变量，否则不会自动生成json格式
+			}
+		}
+		//m = m.Data(updateData) //字段被解析成`table.xxxx`，正确的应该是`table`.`xxxx`
+		//解决字段被解析成`table.xxxx`的BUG
+		fieldArr := []string{}
+		valueArr := []interface{}{}
+		for k, v := range updateData {
+			_, ok := v.(gdb.Raw)
+			if ok {
+				fieldArr = append(fieldArr, k+` = `+gconv.String(v))
+			} else {
+				fieldArr = append(fieldArr, k+` = ?`)
+				valueArr = append(valueArr, v)
+			}
+		}
+		data := []interface{}{strings.Join(fieldArr, `,`)}
+		data = append(data, valueArr...)
+		m = m.Data(data...)
+		return m
+	}
+}
+
+// 解析field
+func (daoThis *actionRelToSceneDao) ParseField(field []string, joinTableArr *[]string) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		afterField := []string{}
+		for _, v := range field {
+			switch v {
+			/* case `xxxx`:
+			m = daoThis.ParseJoin(Xxxx.Table(), joinTableArr)(m)
+			afterField = append(afterField, v) */
+			case `id`:
+				m = m.Fields(daoThis.Table() + `.` + daoThis.PrimaryKey() + ` AS ` + v)
+			case `name`:
+				nameField := gstr.SubStr(gstr.CaseCamel(daoThis.PrimaryKey()), 0, -2) + `Name`
+				if daoThis.ColumnArrG().Contains(gstr.CaseCamelLower(nameField)) {
+					m = m.Fields(daoThis.Table() + `.` + gstr.CaseCamelLower(nameField) + ` AS ` + v)
+				} else if daoThis.ColumnArrG().Contains(gstr.CaseSnakeFirstUpper(nameField)) {
+					m = m.Fields(daoThis.Table() + `.` + gstr.CaseSnakeFirstUpper(nameField) + ` AS ` + v)
+				}
+			default:
+				if daoThis.ColumnArrG().Contains(v) {
+					m = m.Fields(daoThis.Table() + `.` + v)
+				} else {
+					m = m.Fields(v)
+				}
+			}
+		}
+		if len(afterField) > 0 {
+			m = m.Hook(daoThis.AfterField(afterField))
+		}
+		return m
+	}
+}
+
+// 解析filter
+func (daoThis *actionRelToSceneDao) ParseFilter(filter map[string]interface{}, joinTableArr *[]string) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		for k, v := range filter {
+			switch k {
+			case `id`, `idArr`:
+				val := gconv.SliceInt(v)
+				if len(val) == 1 {
+					m = m.Where(daoThis.Table()+`.`+daoThis.PrimaryKey(), val[0])
+				} else {
+					m = m.Where(daoThis.Table()+`.`+daoThis.PrimaryKey(), v)
+				}
+			case `excId`, `excIdArr`:
+				val := gconv.SliceInt(v)
+				switch len(val) {
+				case 0: //gconv.SliceInt会把0转换成[]int{}，故不能用转换后的val。必须用原始数据v
+					m = m.WhereNot(daoThis.Table()+`.`+daoThis.PrimaryKey(), v)
+				case 1:
+					m = m.WhereNot(daoThis.Table()+`.`+daoThis.PrimaryKey(), val[0])
+				default:
+					m = m.WhereNotIn(daoThis.Table()+`.`+daoThis.PrimaryKey(), v)
+				}
+			case `startTime`:
+				m = m.WhereGTE(daoThis.Table()+`.`+daoThis.Columns().CreatedAt, v)
+			case `endTime`:
+				m = m.WhereLTE(daoThis.Table()+`.`+daoThis.Columns().CreatedAt, v)
+			case `name`:
+				nameField := gstr.SubStr(gstr.CaseCamel(daoThis.PrimaryKey()), 0, -2) + `Name`
+				if daoThis.ColumnArrG().Contains(gstr.CaseCamelLower(nameField)) {
+					m = m.WhereLike(daoThis.Table()+`.`+gstr.CaseCamelLower(nameField), `%`+gconv.String(v)+`%`)
+				} else if daoThis.ColumnArrG().Contains(gstr.CaseSnakeFirstUpper(nameField)) {
+					m = m.WhereLike(daoThis.Table()+`.`+gstr.CaseSnakeFirstUpper(nameField), `%`+gconv.String(v)+`%`)
+				}
+			default:
+				kArr := strings.Split(k, ` `) //支持`id > ?`等k
+				if daoThis.ColumnArrG().Contains(kArr[0]) {
+					if len(kArr) == 1 {
+						if gstr.ToLower(gstr.SubStr(kArr[0], -2)) == `id` {
+							val := gconv.SliceInt(v)
+							if len(val) == 1 {
+								m = m.Where(daoThis.Table()+`.`+k, val[0])
+							} else {
+								m = m.Where(daoThis.Table()+`.`+k, v)
+							}
+						} else if gstr.ToLower(gstr.SubStr(kArr[0], -4)) == `name` {
+							m = m.WhereLike(daoThis.Table()+`.`+k, `%`+gconv.String(v)+`%`)
+						} else {
+							m = m.Where(daoThis.Table()+`.`+k, v)
+						}
+					} else {
+						m = m.Where(daoThis.Table()+`.`+k, v)
+					}
+				} else {
+					m = m.Where(k, v)
+				}
+			}
+		}
+		return m
+	}
+}
+
+// 解析group
+func (daoThis *actionRelToSceneDao) ParseGroup(group []string, joinTableArr *[]string) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		for _, v := range group {
+			switch v {
+			case `id`:
+				m = m.Group(daoThis.Table() + `.` + daoThis.PrimaryKey())
+			default:
+				if daoThis.ColumnArrG().Contains(v) {
+					m = m.Group(daoThis.Table() + `.` + v)
+				} else {
+					m = m.Group(v)
+				}
+			}
+		}
+		return m
+	}
+}
+
+// 解析order
+func (daoThis *actionRelToSceneDao) ParseOrder(order []string, joinTableArr *[]string) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		for _, v := range order {
+			kArr := strings.Split(v, ` `)
+			if len(kArr) == 1 {
+				kArr = append(kArr, `ASC`)
+			}
+			switch kArr[0] {
+			case `id`:
+				m = m.Order(daoThis.Table()+`.`+daoThis.PrimaryKey(), kArr[1])
+			default:
+				if daoThis.ColumnArrG().Contains(kArr[0]) {
+					m = m.Order(daoThis.Table()+`.`+kArr[0], kArr[1])
+				} else {
+					m = m.Order(kArr[0], kArr[1])
+				}
+			}
+		}
+		return m
+	}
+}
+
+// 解析join
+func (daoThis *actionRelToSceneDao) ParseJoin(joinCode string, joinTableArr *[]string) gdb.ModelHandler {
+	return func(m *gdb.Model) *gdb.Model {
+		switch joinCode {
+		/* case Xxxx.Table():
+		relTable := Xxxx.Table()
+		if !garray.NewStrArrayFrom(*joinTableArr).Contains(relTable) {
+			*joinTableArr = append(*joinTableArr, relTable)
+			m = m.LeftJoin(relTable, relTable+`.`+daoThis.PrimaryKey()+` = `+daoThis.Table()+`.`+daoThis.PrimaryKey())
+		} */
+		}
+		return m
+	}
+}
+
+// 获取数据后，再处理的字段
+func (daoThis *actionRelToSceneDao) AfterField(afterField []string) gdb.HookHandler {
+	return gdb.HookHandler{
+		Select: func(ctx context.Context, in *gdb.HookSelectInput) (result gdb.Result, err error) {
+			result, err = in.Next(ctx)
+			if err != nil {
+				return
+			}
+			for i, record := range result {
+				for _, v := range afterField {
+					switch v {
+					/* case `xxxx`:
+					record[v] = gvar.New(``) */
+					}
+				}
+				result[i] = record
+			}
+			return
+		},
 	}
 }
 
