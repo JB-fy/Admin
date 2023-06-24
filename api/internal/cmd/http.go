4b8c9fc9a2a1082bfd3c246195783452f41797e4
13599995734 <274456806@qq.com>
2023-06-24 18:29:11 +0800

    1

--- api/internal/cmd/http.go
+++ api/internal/cmd/http.go
@@ -6,16 +6,13 @@ import (
 	"github.com/gogf/gf/v2/container/garray"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
-	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
 	"github.com/gogf/gf/v2/os/genv"
 	"github.com/gogf/gf/v2/os/gtime"
 	"github.com/gogf/gf/v2/util/gvalid"
 
-	"api/internal/controller"
 	"api/internal/corn"
 	daoPlatform "api/internal/dao/platform"
-	"api/internal/middleware"
 	"api/internal/router"
 	"api/internal/utils"
 )
@@ -58,32 +55,8 @@ func HttpFunc(ctx context.Context, parser *gcmd.Parser) (err error) {
 
 	/*--------启动http服务 开始--------*/
 	s := g.Server()
-	//首页
-	s.BindHandler(`/`, func(r *ghttp.Request) {
-		r.Response.RedirectTo(`/view/platform`)
-	})
-	//上传回调
-	s.Group(`/upload`, func(group *ghttp.RouterGroup) {
-		controllerThis := controller.NewUpload()
-		group.Bind(
-			// controllerThis.Sign, //建议放其他场景内验证权限后才可调用
-			controllerThis.Sts, //App端的SDK需设置一个地址来获取Sts Token，该地址不验证权限。需同时在其他场景路由下设置需验证权限的路由，用于给App端获取实际上传时需用到的字段
-			controllerThis.Notify,
-		)
-	})
-	// 测试使用
-	s.Group(``, func(group *ghttp.RouterGroup) {
-		group.Middleware(middleware.Cross, middleware.I18n)
-		// group.Middleware(middleware.HandlerResponse) // 不用规范路由方式可去掉。且如果有用log中间件，必须放在其后面，才能读取到响应数据
-		controllerThis := controller.NewTest()
-		group.ALL(`/test`, controllerThis.Test)
-		group.Bind(
-			controllerThis.TestMeta,
-			//controllerThis,
-			//controller.NewTest().Test, //这样不会根据方法名自动设置路由
-		)
-	})
 
+	router.InitRouterCommon(s)   //公共接口注册
 	router.InitRouterPlatform(s) //平台后台接口注册
 	s.Run()
 	/*--------启动http服务 结束--------*/
---
+++ api/internal/router/common.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"api/internal/controller"
+	"api/internal/middleware"
+
+	"github.com/gogf/gf/v2/net/ghttp"
+)
+
+func InitRouterCommon(s *ghttp.Server) {
+	//首页
+	s.BindHandler(`/`, func(r *ghttp.Request) {
+		r.Response.RedirectTo(`/view/platform`)
+	})
+	//上传
+	s.Group(`/upload`, func(group *ghttp.RouterGroup) {
+		controllerThis := controller.NewUpload()
+		group.Bind(
+			// controllerThis.Sign, //建议放其他场景内验证权限后才可调用
+			controllerThis.Sts, //App端的SDK需设置一个地址来获取Sts Token，该地址不验证权限。需同时在其他场景路由下设置需验证权限的路由，用于给App端获取实际上传时需用到的字段
+			controllerThis.Notify,
+		)
+	})
+	// 测试使用
+	s.Group(``, func(group *ghttp.RouterGroup) {
+		group.Middleware(middleware.Cross, middleware.I18n)
+		// group.Middleware(middleware.HandlerResponse) // 不用规范路由方式可去掉。且如果有用log中间件，必须放在其后面，才能读取到响应数据
+		controllerThis := controller.NewTest()
+		group.ALL(`/test`, controllerThis.Test)
+		group.Bind(controllerThis.TestMeta)
+	})
+}
