5de8ba4bc0be5876d4ca2e7b092d2bbecda038c6
13599995734 <274456806@qq.com>
2023-06-24 11:47:36 +0800

    1

--- api/api/platform/log/http.go
+++ api/api/platform/log/http.go
@@ -1,18 +1,54 @@
 package api
 
 import (
-	apiCommon "api/api"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
 )
 
+/*--------列表 开始--------*/
 type HttpListReq struct {
-	apiCommon.CommonListReq
-	Filter HttpListFilterReq `p:"filter"`
+	g.Meta `path:"/list" method:"post" tags:"平台-角色" sm:"列表"`
+	Filter HttpListFilter `json:"filter" dc:"过滤条件"`
+	// apiCommon.CommonListReq
+	Field []string `json:"field" v:"distinct|foreach|min-length:1" dc:"查询字段。默认会返回全部查询字段。如果需要的字段较少，建议指定字段，传值参考默认返回的字段"`
+	Sort  string   `json:"sort" default:"id DESC" dc:"排序"`
+	Page  int      `json:"page" v:"integer|min:1" default:"1" dc:"页码"`
+	Limit int      `json:"limit" v:"integer|min:0" default:"10" dc:"每页数量。可传0取全部"`
 }
 
-type HttpListFilterReq struct {
-	apiCommon.CommonListFilterReq `c:",omitempty"`
-	HttpId                        *uint    `c:"httpId,omitempty" p:"httpId" v:"integer|min:1"`
-	Url                           string   `c:"url,omitempty" p:"url" v:"url"`
-	MinRunTime                    *float64 `c:"minRunTime,omitempty" p:"minRunTime" v:"float|min:0"`
-	MaxRunTime                    *float64 `c:"maxRunTime,omitempty" p:"maxRunTime" v:"float|min:0|gte:MinRunTime"`
+type HttpListFilter struct {
+	/*--------公共参数 开始--------*/
+	// apiCommon.CommonListFilterReq `c:",omitempty"`	// 代码中用到转换成map，且必须用omitempty过滤空参数。而规范路由自动生成swagger会因omitempty导致这些字段不生成。故直接写这里
+	Id        *uint       `c:"id,omitempty" json:"id" v:"integer|min:1" dc:"ID"`
+	IdArr     []uint      `c:"idArr,omitempty" json:"idArr" v:"distinct|foreach|integer|foreach|min:1" dc:"ID数组"`
+	ExcId     *uint       `c:"excId,omitempty" json:"excId" v:"integer|min:1" dc:"排除ID"`
+	ExcIdArr  []uint      `c:"excIdArr,omitempty" json:"excIdArr" v:"distinct|foreach|integer|foreach|min:1" dc:"排除ID数组"`
+	StartTime *gtime.Time `c:"startTime,omitempty" json:"startTime" v:"date-format:Y-m-d H:i:s" dc:"开始时间。示例：2000-01-01 00:00:00"`
+	EndTime   *gtime.Time `c:"endTime,omitempty" json:"endTime" v:"date-format:Y-m-d H:i:s|after-equal:StartTime" dc:"结束时间。示例：2000-01-01 00:00:00"`
+	Name      string      `c:"name,omitempty" json:"name" v:"length:1,30|regex:^[\\p{L}\\p{M}\\p{N}_-]+$" dc:"名称。后台公共列表常用"`
+	/*--------公共参数 结束--------*/
+	HttpId     *uint    `c:"httpId,omitempty" json:"httpId" v:"integer|min:1" dc:"Http记录ID"`
+	Url        string   `c:"url,omitempty" json:"url" v:"url" dc:"地址"`
+	MinRunTime *float64 `c:"minRunTime,omitempty" json:"minRunTime" v:"float|min:0" dc:"最小运行时间"`
+	MaxRunTime *float64 `c:"maxRunTime,omitempty" json:"maxRunTime" v:"float|min:0|gte:MinRunTime" dc:"最大运行时间"`
 }
+
+type HttpListRes struct {
+	// apiCommon.CommonListRes
+	Count int        `json:"count" dc:"总数"`
+	List  []HttpList `json:"list" dc:"列表"`
+}
+
+type HttpList struct {
+	Id        uint    `json:"id" dc:"ID"`
+	HttpId    uint    `json:"httpId" dc:"Http记录ID"`
+	Url       string  `json:"url" dc:"地址"`
+	Header    string  `json:"header" dc:"请求头"`
+	ReqData   string  `json:"reqData" dc:"请求数据"`
+	ResData   string  `json:"resData" dc:"响应数据"`
+	RunTime   float64 `json:"runTime" dc:"运行时间（单位：毫秒）"`
+	UpdatedAt string  `json:"updatedAt" dc:"更新时间"`
+	CreatedAt string  `json:"createdAt" dc:"创建时间"`
+}
+
+/*--------列表 结束--------*/
--- api/internal/controller/platform/log/http.go
+++ api/internal/controller/platform/log/http.go
@@ -4,10 +4,9 @@ import (
 	apiLog "api/api/platform/log"
 	daoLog "api/internal/dao/log"
 	"api/internal/service"
-	"api/internal/utils"
+	"context"
 
 	"github.com/gogf/gf/v2/container/gset"
-	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/util/gconv"
 )
 
@@ -18,54 +17,46 @@ func NewHttp() *Http {
 }
 
 // 列表
-func (controllerThis *Http) List(r *ghttp.Request) {
+func (controllerThis *Http) List(ctx context.Context, req *apiLog.HttpListReq) (res *apiLog.HttpListRes, err error) {
 	/**--------参数处理 开始--------**/
-	var param *apiLog.HttpListReq
-	err := r.Parse(&param)
-	if err != nil {
-		utils.HttpFailJson(r, utils.NewErrorCode(r.GetCtx(), 89999999, err.Error()))
-		return
-	}
-	filter := gconv.Map(param.Filter)
+	filter := gconv.Map(req.Filter)
 	if filter == nil {
 		filter = map[string]interface{}{}
 	}
-	order := []string{param.Sort}
-	page := param.Page
-	limit := param.Limit
+	order := []string{req.Sort}
+	page := req.Page
+	limit := req.Limit
+
+	allowField := daoLog.Http.ColumnArr()
+	allowField = append(allowField, `id`)
+	field := allowField
+	if len(req.Field) > 0 {
+		field = gset.NewStrSetFrom(req.Field).Intersect(gset.NewStrSetFrom(allowField)).Slice()
+		if len(field) == 0 {
+			field = allowField
+		}
+	}
 	/**--------参数处理 结束--------**/
 
-	sceneCode := utils.GetCtxSceneCode(r.GetCtx())
-	switch sceneCode {
-	case `platform`:
-		/**--------权限验证 开始--------**/
-		_, err := service.Action().CheckAuth(r.GetCtx(), `logHttpLook`)
-		if err != nil {
-			utils.HttpFailJson(r, err)
-			return
-		}
-		allowField := daoLog.Http.ColumnArr()
-		allowField = append(allowField, `id`)
-		//allowField = gset.NewStrSetFrom(allowField).Diff(gset.NewStrSetFrom([]string{`password`})).Slice() //移除敏感字段
-		field := allowField
-		if len(param.Field) > 0 {
-			field = gset.NewStrSetFrom(param.Field).Intersect(gset.NewStrSetFrom(allowField)).Slice()
-			if len(field) == 0 {
-				field = allowField
-			}
-		}
-		/**--------权限验证 结束--------**/
-
-		count, err := service.Http().Count(r.GetCtx(), filter)
-		if err != nil {
-			utils.HttpFailJson(r, err)
-			return
-		}
-		list, err := service.Http().List(r.GetCtx(), filter, field, order, page, limit)
-		if err != nil {
-			utils.HttpFailJson(r, err)
-			return
-		}
-		utils.HttpSuccessJson(r, map[string]interface{}{`count`: count, `list`: list}, 0)
+	/**--------权限验证 开始--------**/
+	_, err = service.Action().CheckAuth(ctx, `logHttpLook`)
+	if err != nil {
+		return
 	}
+	/**--------权限验证 结束--------**/
+
+	count, err := service.Http().Count(ctx, filter)
+	if err != nil {
+		return
+	}
+	list, err := service.Http().List(ctx, filter, field, order, page, limit)
+	if err != nil {
+		return
+	}
+	res = &apiLog.HttpListRes{
+		Count: count,
+	}
+	list.Structs(&res.List)
+	// utils.HttpSuccessJson(g.RequestFromCtx(ctx), map[string]interface{}{`count`: count, `list`: list}, 0)
+	return
 }
