584c7282917e671f7e98d281f824be8f487c237e
13599995734 <274456806@qq.com>
2023-07-01 00:00:05 +0800

    1

--- api/internal/corn/corn.go
+++ api/internal/corn/corn.go
@@ -2,20 +2,35 @@ package corn
 
 import (
 	daoLog "api/internal/dao/log"
+	daoPlatform "api/internal/dao/platform"
 	"api/internal/utils"
 	"context"
+	"fmt"
 
 	"github.com/gogf/gf/v2/os/gcron"
 )
 
+var JobList = map[string]func(ctx context.Context){
+	`LogHttpPartition`: LogHttpPartition,
+	`Test`:             Test,
+}
+
 func InitCorn(ctx context.Context) {
-	// 星期一的凌晨3点执行
-	gcron.Add(ctx, `0 0 3 * * 1`, func(ctx context.Context) {
-		LogHttpPartition(ctx)
-	}, `LogHttpPartition`)
+	columns := daoPlatform.Corn.Columns()
+	cornList, _ := daoPlatform.Corn.ParseDbCtx(ctx).Where(columns.IsStop, 0).All()
+	for _, corn := range cornList {
+		code := corn[columns.CornCode].String()
+		pattern := corn[columns.CornPattern].String()
+		gcron.AddSingleton(ctx, pattern, JobList[code], code)
+	}
 }
 
 // Http日志表每周新增分区
 func LogHttpPartition(ctx context.Context) {
-	utils.DbTablePartition(ctx, daoLog.Http.Group(), daoLog.Http.Table(), 7, 24*60*60, `createdAt`)
+	utils.DbTablePartition(ctx, daoLog.Http.Group(), daoLog.Http.Table(), 7, 24*60*60, daoLog.Http.Columns().CreatedAt)
+}
+
+// 测试
+func Test(ctx context.Context) {
+	fmt.Println(1111)
 }
