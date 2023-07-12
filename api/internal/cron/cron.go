0b64194fb762796e074a15fe8d641b18e1a7c1b8
13599995734 <274456806@qq.com>
2023-07-12 18:05:58 +0800

    1

--- api/internal/cron/cron.go
+++ api/internal/cron/cron.go
@@ -5,27 +5,33 @@ import (
 	daoPlatform "api/internal/dao/platform"
 	"api/internal/utils"
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/gogf/gf/v2/os/gcron"
 )
 
-var JobList = map[string]func(ctx context.Context){
-	`LogHttpPartition`: LogHttpPartition,
-	`Test`:             Test,
-}
-
 func InitCron(ctx context.Context) {
 	columns := daoPlatform.Cron.Columns()
 	cronList, _ := daoPlatform.Cron.ParseDbCtx(ctx).Where(columns.IsStop, 0).All()
 	for _, cron := range cronList {
-		code := cron[columns.CronCode].String()
-		pattern := cron[columns.CronPattern].String()
-		_, ok := JobList[code]
-		if ok {
-			gcron.AddSingleton(ctx, pattern, JobList[code], code)
-		}
+		CronStart(ctx, cron[columns.CronPattern].String(), cron[columns.CronCode].String())
 	}
+}
+
+// 开启定时任务
+func CronStart(ctx context.Context, pattern string, code string) (err error) {
+	var JobList = map[string]func(ctx context.Context){
+		`LogHttpPartition`: LogHttpPartition,
+		`Test`:             Test,
+	}
+	_, ok := JobList[code]
+	if !ok {
+		err = errors.New(`定时器方法不存在`)
+		return
+	}
+	gcron.AddSingleton(ctx, pattern, JobList[code], code)
+	return
 }
 
 // Http日志表每周新增分区
