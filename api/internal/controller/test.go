1813b703fa26003fd9573526ced455f627a900f9
13599995734 <274456806@qq.com>
2023-07-13 17:38:18 +0800

    1

--- api/internal/controller/test.go
+++ api/internal/controller/test.go
@@ -31,18 +31,34 @@ func (c *Test) TestMeta(ctx context.Context, req *api.TestMetaReq) (res *api.Tes
 	// fmt.Println(g.RequestFromCtx(ctx).Router)     // &{/testMeta GET default ^/testMeta$ [] 0}
 	// fmt.Println(g.RequestFromCtx(ctx).Router.Uri) // /testMeta
 
-	// utils.HttpFailJson(g.RequestFromCtx(ctx), utils.NewErrorCode(ctx, 99999999, ``))
+	// fmt.Println(grand.N(1000, 9999))
+	// fmt.Println(grand.Intn(1))
+	// fmt.Println(grand.Str(`abcdefg0123456789`, 8))
+	// fmt.Println(grand.S(8))       //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+	// fmt.Println(grand.Digits(8))  //0123456789
+	// fmt.Println(grand.Letters(8)) //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+	// fmt.Println(grand.Symbols(8)) //!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+
+	// g.Log().Info(ctx, `日志打印`)
+	// fmt.Println(g.I18n().T(ctx, `code.99999999`))
+
+	// fmt.Println(garray.NewStrArrayFrom([]string{`a`, `b`, `c`}).Contains(`a`))	//是否含有元素
+
+	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Diff(gset.NewIntSetFrom([]int{1, 3})).Slice())	//差集
+
+	// fmt.Println(genv.Set(`X_X`, `xx`))              //key必须由大写和_组成
+	// fmt.Println(g.Cfg().MustGetWithEnv(ctx, `X_X`)) //X_X或x_x或x.x方法都可以读取到
+	// fmt.Println(g.Cfg().MustGet(ctx, `superPlatformAdminId`).Int())	//获取配置参数
+
+	//fmt.Println(ghttp.RestartAllServer(ctx))	//重启服务
+
+	/* utils.HttpWriteJson(ctx, map[string]interface{}{
+		`info`: map[string]interface{}{},
+	}, 0, ``) */
 	// g.RequestFromCtx(ctx).Response.Status = http.StatusMultipleChoices
 	res = &api.TestMetaRes{
 		Test: `测试`,
 	}
-	/* g.RequestFromCtx(ctx).Response.WriteJson(map[string]interface{}{
-		`code`: 0,
-		`msg`:  g.I18n().T(ctx, `code.0`),
-		`data`: map[string]interface{}{
-			`list`: []map[string]interface{}{},
-		},
-	}) */
 	return
 }
 
@@ -53,30 +69,6 @@ func (c *Test) Test(r *ghttp.Request) {
 		r.Response.Writeln(err.Error())
 		return
 	} */
-
-	// g.Log().Info(r.GetCtx(), `日志打印`)
-	// fmt.Println(g.I18n().T(ctx, `code.99999999`))
-
-	// fmt.Println(garray.NewStrArrayFrom([]string{`a`, `b`, `c`}).Contains(`a`))
-
-	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Diff(gset.NewIntSetFrom([]int{1, 3})).Slice())
-
-	// fmt.Println(grand.N(1000, 9999))
-	// fmt.Println(grand.Intn(1))
-	// fmt.Println(grand.Str(`abcdefg0123456789`, 8))
-	// fmt.Println(grand.S(8))       //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
-	// fmt.Println(grand.Digits(8))  //0123456789
-	// fmt.Println(grand.Letters(8)) //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-	// fmt.Println(grand.Symbols(8)) //!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
-
-	// fmt.Println(daoAuth.RoleRelToMenu.ParseDbCtx(r.GetCtx()).Where(`roleId`, 1).Array(`menuId`))
-
-	// fmt.Println(genv.Set(`X_X`, `xx`))                     //key必须由大写和_组成
-	// fmt.Println(g.Cfg().MustGetWithEnv(r.GetCtx(), `X_X`)) //X_X或x_x或x.x方法都可以读取到
-
-	// fmt.Println(g.Cfg().MustGet(r.GetCtx(), `superPlatformAdminId`).Int())
-
-	//fmt.Println(ghttp.RestartAllServer(r.GetCtx()))
 
 	// r.SetError(utils.NewErrorCode(r.GetCtx(), 99999999, ``))
 	r.Response.WriteJson(map[string]interface{}{
