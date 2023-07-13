c2412bd8a04b5c040314a356eba527d6f5896c12
13599995734 <274456806@qq.com>
2023-07-13 16:33:25 +0800

    1

--- api/internal/initialize/genv.go
+++ api/internal/initialize/genv.go
@@ -12,6 +12,7 @@ import (
 )
 
 func InitGenv(ctx context.Context) {
+	/**--------记录当前服务器IP 开始--------**/
 	serverNetworkIp := utils.GetServerNetworkIp()
 	serverLocalIp := utils.GetServerLocalIp()
 	genv.Set(`SERVER_NETWORK_IP`, serverNetworkIp) //设置服务器外网ip（key必须由大写和_组成，才能用g.Cfg().MustGetWithEnv()方法读取）
@@ -21,4 +22,5 @@ func InitGenv(ctx context.Context) {
 		daoPlatform.Server.Columns().NetworkIp: serverNetworkIp,
 		daoPlatform.Server.Columns().LocalIp:   serverLocalIp,
 	}).Save()
+	/**--------记录当前服务器IP 结束--------**/
 }
