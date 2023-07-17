59067b2e497013cc796386118ead79d3b8b735ac
13599995734 <274456806@qq.com>
2023-07-17 18:17:16 +0800

    1

--- api/internal/logic/index/login.go
+++ api/internal/logic/index/login.go
@@ -31,39 +31,36 @@ func (logicThis *sLogin) EncryptStr(ctx context.Context, sceneCode string, accou
 	return
 }
 
-// 登录
-func (logicThis *sLogin) Login(ctx context.Context, sceneCode string, account string, password string) (token string, err error) {
-	switch sceneCode {
-	case `platform`:
-		/**--------验证账号密码 开始--------**/
-		info, _ := daoPlatform.Admin.ParseDbCtx(ctx).Handler(daoPlatform.Admin.ParseFilter(map[string]interface{}{`accountOrPhone`: account}, &[]string{})).One()
-		if len(info) == 0 {
-			err = utils.NewErrorCode(ctx, 39990000, ``)
-			return
-		}
-		if info[`isStop`].Int() > 0 {
-			err = utils.NewErrorCode(ctx, 39990001, ``)
-			return
-		}
-		encryptStrKey := fmt.Sprintf(consts.CacheEncryptStrFormat, sceneCode, account)
-		encryptStr, _ := g.Redis().Get(ctx, encryptStrKey)
-		if encryptStr.String() == `` || gmd5.MustEncrypt(info[`password`].String()+encryptStr.String()) != password {
-			err = utils.NewErrorCode(ctx, 39990000, ``)
-			return
-		}
-		/**--------验证账号密码 结束--------**/
-
-		claims := utils.CustomClaims{
-			LoginId:  info[`adminId`].Uint(),
-			Account:  info[`account`].String(),
-			Nickname: info[`nickname`].String(),
-		}
-		jwt := utils.NewJWT(ctx, utils.GetCtxSceneInfo(ctx)[`sceneConfig`].Map())
-		token, err = jwt.CreateToken(claims)
-		/* //缓存token（选做。限制多地登录，多设备登录等情况下可用）
-		TokenKey := fmt.Sprintf(consts.CacheTokenFormat, sceneCode, claims.Account)
-		g.Redis().SetEX(ctx, TokenKey, token, int64(jwt.ExpireTime)) */
+// 平台后台登录
+func (logicThis *sLogin) PlatformLogin(ctx context.Context, account string, password string) (token string, err error) {
+	sceneCode := `platform` //指定场景
+	/**--------验证账号密码 开始--------**/
+	info, _ := daoPlatform.Admin.ParseDbCtx(ctx).Handler(daoPlatform.Admin.ParseFilter(map[string]interface{}{`accountOrPhone`: account}, &[]string{})).One()
+	if len(info) == 0 {
+		err = utils.NewErrorCode(ctx, 39990000, ``)
 		return
 	}
+	if info[`isStop`].Int() > 0 {
+		err = utils.NewErrorCode(ctx, 39990001, ``)
+		return
+	}
+	encryptStrKey := fmt.Sprintf(consts.CacheEncryptStrFormat, sceneCode, account)
+	encryptStr, _ := g.Redis().Get(ctx, encryptStrKey)
+	if encryptStr.String() == `` || gmd5.MustEncrypt(info[`password`].String()+encryptStr.String()) != password {
+		err = utils.NewErrorCode(ctx, 39990000, ``)
+		return
+	}
+	/**--------验证账号密码 结束--------**/
+
+	claims := utils.CustomClaims{
+		LoginId:  info[`adminId`].Uint(),
+		Account:  info[`account`].String(),
+		Nickname: info[`nickname`].String(),
+	}
+	jwt := utils.NewJWT(ctx, utils.GetCtxSceneInfo(ctx)[`sceneConfig`].Map())
+	token, err = jwt.CreateToken(claims)
+	/* //缓存token（选做。限制多地登录，多设备登录等情况下可用）
+	TokenKey := fmt.Sprintf(consts.CacheTokenFormat, sceneCode, claims.Account)
+	g.Redis().SetEX(ctx, TokenKey, token, int64(jwt.ExpireTime)) */
 	return
 }
