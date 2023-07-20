ff77e59a57fec24d023b9868bc09175e2127c19f
13599995734 <274456806@qq.com>
2023-07-20 10:38:59 +0800

    1

--- dev/platform/src/i18n/language/zh-cn/config.ts
+++ dev/platform/src/i18n/language/zh-cn/config.ts
@@ -1,4 +1,4 @@
-// 配置文件（不用翻译。主要作用方便在HTML代码中使用，HTML不支持直接写import.meta.env.VITE_HTTP_API_PREFIX）
+// 当配置文件用（不用翻译。方便在HTML代码中使用，HTML不支持直接用import.meta.env.XXXX）
 export default {
     VITE_HTTP_API_PREFIX: import.meta.env.VITE_HTTP_API_PREFIX
 }
