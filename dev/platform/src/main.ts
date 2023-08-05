41495c0c99be2781779a5ac7a8a08a2a90c43837
JB <274456806@qq.com>
2023-08-05 22:22:45 +0800

    1

--- dev/platform/src/main.ts
+++ dev/platform/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 
-import "./assets/main.css"
+import "./assets/css/main.css"
 
 import i18n from './i18n'
 import router from './router'
