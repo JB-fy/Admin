2c1749cd7e2dec6f0a952e786bcb83b68090c51f
JB <274456806@qq.com>
2023-08-09 15:42:09 +0800

    1

--- api/internal/consts/consts.go
+++ api/internal/consts/consts.go
@@ -1,9 +1,9 @@
 package consts
 
 var (
-	CacheSaltFormat  = "salt_%s_%s"  //加密盐缓存key。参数：场景标识，账号
-	CacheTokenFormat = "token_%s_%s" //登录后的token缓存key。参数：场景标识，用户标识
+	CacheSaltFormat  = `salt_%s_%s`  //加密盐缓存key。参数：场景标识，账号
+	CacheTokenFormat = `token_%s_%s` //登录后的token缓存key。参数：场景标识，用户标识
 
-	ConstCtxSceneInfoName = "sceneInfo"
-	ConstCtxLoginInfoName = "loginInfo"
+	ConstCtxSceneInfoName = `sceneInfo`
+	ConstCtxLoginInfoName = `loginInfo`
 )
