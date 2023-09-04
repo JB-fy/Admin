a9d88fc7092bca026012d3b37a788eb204317589
JB <274456806@qq.com>
2023-09-04 16:19:16 +0800

    1

--- api/internal/middleware/cross.go
+++ api/internal/middleware/cross.go
@@ -5,6 +5,21 @@ import (
 )
 
 func Cross(r *ghttp.Request) {
+	//也可以在nginx中直接设置全站跨域
+	/* location / {
+	    add_header Access-Control-Allow-Credentials true;
+	    #add_header Access-Control-Allow-Origin $http_origin;
+	    #add_header Access-Control-Allow-Origin 'http://www.xxxx.com';
+	    add_header Access-Control-Allow-Origin *;
+	    #add_header Access-Control-Allow-Methods 'GET, POST, PUT, DELETE, PATCH, OPTIONS';
+	    add_header Access-Control-Allow-Methods *;
+	    #add_header Access-Control-Allow-Headers 'X-Requested-With, Content-Type, Accept, Origin, Authorization';
+	    add_header Access-Control-Allow-Headers *;
+	    if ($request_method = 'OPTIONS') {
+	        return 204;
+	    }
+	} */
+
 	/* corsOptions := r.Response.DefaultCORSOptions()
 	corsOptions.AllowHeaders = `*`
 	corsOptions.AllowMethods = `*`
