a06989ad9e70cad37c46049bb01b1c9918dd75f7
JB <274456806@qq.com>
2023-09-12 16:34:18 +0800

    1

---
+++ api/internal/utils/sms/aliyun_sms.go
@@ -0,0 +1,95 @@
+package sms
+
+/* import (
+	"context"
+	"errors"
+	"strings"
+
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v3/client"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/gogf/gf/v2/util/gconv"
+)
+
+type AliyunSms struct {
+	Ctx             context.Context
+	AccessKeyId     string `json:"aliyunSmsAccessKeyId"`
+	AccessKeySecret string `json:"aliyunSmsAccessKeySecret"`
+	SignName        string `json:"aliyunSmsSignName"`
+	TemplateCode    string `json:"aliyunSmsTemplateCode"`
+}
+
+func NewAliyunSms(ctx context.Context, config map[string]interface{}) *AliyunSms {
+	aliyunSmsObj := AliyunSms{
+		Ctx: ctx,
+	}
+	gconv.Struct(config, &aliyunSmsObj)
+	return &aliyunSmsObj
+}
+
+func (smsThis *AliyunSms) Send(ctx context.Context, phone string, code string) (err error) {
+	err = smsThis.SendSms(ctx, []string{phone}, `{"code": "`+code+`"}`)
+	return
+}
+
+func (smsThis *AliyunSms) SendSms(ctx context.Context, phoneArr []string, templateParam string) (err error) {
+	client, err := smsThis.CreateClient()
+	if err != nil {
+		return
+	}
+
+	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
+		PhoneNumbers:  tea.String(strings.Join(phoneArr, `,`)),
+		SignName:      tea.String(smsThis.SignName),
+		TemplateCode:  tea.String(smsThis.TemplateCode),
+		TemplateParam: tea.String(templateParam),
+		// TemplateParam: tea.String(`{"code": "1234"}`),
+	}
+
+	tryErr := func() (_e error) {
+		defer func() {
+			r := tea.Recover(recover())
+			if r != nil {
+				_e = r
+			}
+		}()
+		result, err := client.SendSmsWithOptions(sendSmsRequest, &util.RuntimeOptions{})
+		if err != nil {
+			return err
+		}
+		if *result.Body.Code != `OK` {
+			err = errors.New(*result.Body.Message)
+			return err
+		}
+		return nil
+	}()
+
+	if tryErr != nil {
+		var errSDK = &tea.SDKError{}
+		_t, ok := tryErr.(*tea.SDKError)
+		if ok {
+			errSDK = _t
+		} else {
+			errSDK.Message = tea.String(tryErr.Error())
+		}
+		errMsg, errSms := util.AssertAsString(errSDK.Message)
+		if errSms != nil {
+			err = errSms
+			return
+		}
+		err = errors.New(*errMsg)
+	}
+	return
+}
+
+func (smsThis *AliyunSms) CreateClient() (client *dysmsapi20170525.Client, err error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(smsThis.AccessKeyId),
+		AccessKeySecret: tea.String(smsThis.AccessKeySecret),
+		Endpoint:        tea.String(`dysmsapi.aliyuncs.com`),
+	}
+
+	client, err = dysmsapi20170525.NewClient(config)
+	return
+} */
---
+++ api/internal/utils/sms/sms.go
@@ -0,0 +1,25 @@
+package sms
+
+/* import (
+	daoPlatform "api/internal/dao/platform"
+	"context"
+)
+
+type Sms interface {
+	Send(ctx context.Context, phone string, code string) (err error)
+	SendSms(ctx context.Context, phoneArr []string, templateParam string) (err error)
+}
+
+func NewSms(ctx context.Context) Sms {
+	platformConfigColumns := daoPlatform.Config.Columns()
+	smsTypeTmp, _ := daoPlatform.Config.ParseDbCtx(ctx).Where(platformConfigColumns.ConfigKey, `smsType`).Value(platformConfigColumns.ConfigValue)
+	smsType := smsTypeTmp.String()
+	switch smsType {
+	case `aliyunSms`:
+		config, _ := daoPlatform.Config.Get(ctx, []string{`aliyunSmsAccessKeyId`, `aliyunSmsAccessKeySecret`, `aliyunSmsSignName`, `aliyunSmsTemplateCode`})
+		return NewAliyunSms(ctx, config)
+	default:
+		config, _ := daoPlatform.Config.Get(ctx, []string{`aliyunSmsAccessKeyId`, `aliyunSmsAccessKeySecret`, `aliyunSmsSignName`, `aliyunSmsTemplateCode`})
+		return NewAliyunSms(ctx, config)
+	}
+} */
