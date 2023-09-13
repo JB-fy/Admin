8bfd04946a0f7033480f48e3373bd9adfe196564
JB <274456806@qq.com>
2023-09-13 18:30:37 +0800

    1

--- dev/platform/src/views/platform/config/Index.vue
+++ dev/platform/src/views/platform/config/Index.vue
@@ -8,7 +8,7 @@ export default {
 <!--直接批量导入组件 结束-->
 <script setup lang="ts">
 //import UploadConfig from './tabPane/UploadConfig.vue'
-//好处：该组件会被打包成单独一个文件
+//下面方式引入好处：该组件会被打包成单独一个文件
 const WebsiteConfig = defineAsyncComponent(() => import('./tabPane/WebsiteConfig.vue'))
 const UploadConfig = defineAsyncComponent(() => import('./tabPane/UploadConfig.vue'))
 const SmsConfig = defineAsyncComponent(() => import('./tabPane/SmsConfig.vue'))
@@ -21,9 +21,6 @@ const { t } = useI18n()
     <ElContainer class="common-container">
         <ElMain>
             <ElTabs type="border-card" tab-position="top">
-                <!-- <ElTabPane :label="t('platform.config.label.testConfig')" :lazy="true">
-                    <UploadConfig />
-                </ElTabPane> -->
                 <ElTabPane :label="t('platform.config.label.websiteConfig')" :lazy="true">
                     <WebsiteConfig />
                 </ElTabPane>
