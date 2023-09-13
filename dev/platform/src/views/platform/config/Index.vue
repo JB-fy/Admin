befca54c0352c295cd8022f12a8807892a3e42f9
JB <274456806@qq.com>
2023-09-13 18:30:47 +0800

    1

--- dev/platform/src/views/platform/config/Index.vue
+++ dev/platform/src/views/platform/config/Index.vue
@@ -8,7 +8,7 @@ export default {
 <!--直接批量导入组件 结束-->
 <script setup lang="ts">
 //import UploadConfig from './tabPane/UploadConfig.vue'
-//下面方式引入好处：该组件会被打包成单独一个文件
+//下面方式引入好处：组件会被打包成单独一个文件
 const WebsiteConfig = defineAsyncComponent(() => import('./tabPane/WebsiteConfig.vue'))
 const UploadConfig = defineAsyncComponent(() => import('./tabPane/UploadConfig.vue'))
 const SmsConfig = defineAsyncComponent(() => import('./tabPane/SmsConfig.vue'))
