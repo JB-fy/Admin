dfb0c67637df1390e7f3692c5824123c449c180c
JB <274456806@qq.com>
2023-10-14 15:32:20 +0800

    1

---
+++ api/internal/model/do/user/user.go
@@ -0,0 +1,30 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package do
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
+)
+
+// User is the golang structure of table user for DAO operations like Where/Data.
+type User struct {
+	g.Meta     `orm:"table:user, do:true"`
+	UserId     interface{} // 用户ID
+	Phone      interface{} // 手机
+	Account    interface{} // 账号
+	Password   interface{} // 密码（md5保存）
+	Salt       interface{} // 加密盐
+	Nickname   interface{} // 昵称
+	Avatar     interface{} // 头像
+	Gender     interface{} // 性别：0未设置 1男 2女
+	Birthday   *gtime.Time // 生日
+	Address    interface{} // 详细地址
+	IdCardName interface{} // 身份证姓名
+	IdCardNo   interface{} // 身份证号码
+	IsStop     interface{} // 停用：0否 1是
+	UpdatedAt  *gtime.Time // 更新时间
+	CreatedAt  *gtime.Time // 创建时间
+}
---
+++ api/internal/model/entity/user/user.go
@@ -0,0 +1,28 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package entity
+
+import (
+	"github.com/gogf/gf/v2/os/gtime"
+)
+
+// User is the golang structure for table user.
+type User struct {
+	UserId     uint        `json:"userId"     ` // 用户ID
+	Phone      string      `json:"phone"      ` // 手机
+	Account    string      `json:"account"    ` // 账号
+	Password   string      `json:"password"   ` // 密码（md5保存）
+	Salt       string      `json:"salt"       ` // 加密盐
+	Nickname   string      `json:"nickname"   ` // 昵称
+	Avatar     string      `json:"avatar"     ` // 头像
+	Gender     uint        `json:"gender"     ` // 性别：0未设置 1男 2女
+	Birthday   *gtime.Time `json:"birthday"   ` // 生日
+	Address    string      `json:"address"    ` // 详细地址
+	IdCardName string      `json:"idCardName" ` // 身份证姓名
+	IdCardNo   string      `json:"idCardNo"   ` // 身份证号码
+	IsStop     uint        `json:"isStop"     ` // 停用：0否 1是
+	UpdatedAt  *gtime.Time `json:"updatedAt"  ` // 更新时间
+	CreatedAt  *gtime.Time `json:"createdAt"  ` // 创建时间
+}
