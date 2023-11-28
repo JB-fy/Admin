792b1ed101a076c5891558f331e713c8dd319127
JB <274456806@qq.com>
2023-11-28 11:50:44 +0800

    1

--- api/internal/controller/test.go
+++ api/internal/controller/test.go
@@ -15,21 +15,48 @@ func NewTest() *Test {
 }
 
 func (c *Test) Test(ctx context.Context, req *api.TestReq) (res *api.TestRes, err error) {
-	// time.Sleep(10 * time.Second)
+	// fmt.Println(garray.NewStrArrayFrom([]string{`a`, `b`, `c`}).Contains(`a`))	//是否含有元素
 
-	// fmt.Println(g.RequestFromCtx(ctx).GetUrl())   // http://192.168.2.200:20080/testMeta?a=1&b=2
-	// fmt.Println(g.RequestFromCtx(ctx).GetHost())  // 192.168.2.200
-	// fmt.Println(g.RequestFromCtx(ctx).Host)       // 192.168.2.200:20080
-	// fmt.Println(g.RequestFromCtx(ctx).RequestURI) // /testMeta?a=1&b=2
+	/* //合并Map
+	// fmt.Println(gmap.NewStrAnyMapFrom(g.Map{`a`: 1, `b`: 2}).Merge(gmap.NewStrAnyMapFrom(g.Map{`a`: 4, `c`: 3})).Map())
+	rawMap := gmap.NewStrAnyMapFrom(g.Map{`a`: 1, `b`: 2})
+	rawMap.Merge(gmap.NewStrAnyMapFrom(g.Map{`a`: 4, `c`: 3}))
+	fmt.Println(rawMap.Map()) */
 
+	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Intersect(gset.NewIntSetFrom([]int{1, 3})).Slice()) //交集
+	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Diff(gset.NewIntSetFrom([]int{1, 3})).Slice())      //差集
+	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Union(gset.NewIntSetFrom([]int{1, 3})).Slice())     //并集
+	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Merge(gset.NewIntSetFrom([]int{1, 3})).Slice())     //合并，也是并集
+
+	// fmt.Println(g.DB(`default`).Model(`tab_user_unsubscribe`).Safe().Ctx(ctx))	//数据库连接
+
+	// g.Validator().Rules(`required|integer`).Data(`aaaa`).Run(ctx) //单独验证
+
+	// fmt.Println(genv.Set(`X_X`, `xx`))              //key必须由大写和_组成
+	// fmt.Println(g.Cfg().MustGetWithEnv(ctx, `X_X`)) //X_X或x_x或x.x方法都可以读取到
+	// fmt.Println(g.Cfg().MustGet(ctx, `superPlatformAdminId`).Uint())	//获取配置参数
+
+	// g.Log().Info(ctx, `日志打印`, "\r\n", reqData)
+	// fmt.Println(g.I18n().T(ctx, `code.99999999`))
+
+	// time.Sleep(10 * time.Second)	//睡眠几秒
+
+	// fmt.Println(ghttp.RestartAllServer(ctx))	//重启服务
+
+	// fmt.Println(g.RequestFromCtx(ctx).GetClientIp()) // 192.168.2.44。常用于WEB编程，如HTTP
+	// fmt.Println(g.RequestFromCtx(ctx).GetRemoteIp()) // 192.168.2.44。常用于网络编程，如TCP或UDP
+
+	// fmt.Println(g.RequestFromCtx(ctx).GetUrl())         // http://192.168.2.200:20080/testMeta?a=1&b=2
+	// fmt.Println(g.RequestFromCtx(ctx).GetHost())        // 192.168.2.200
+	// fmt.Println(g.RequestFromCtx(ctx).Host)             // 192.168.2.200:20080
+	// fmt.Println(g.RequestFromCtx(ctx).RequestURI)       // /testMeta?a=1&b=2
 	// fmt.Println(g.RequestFromCtx(ctx).URL.String())     // /testMeta?a=1&b=2
 	// fmt.Println(g.RequestFromCtx(ctx).URL)              // /testMeta?a=1&b=2
 	// fmt.Println(g.RequestFromCtx(ctx).URL.Path)         // /testMeta
 	// fmt.Println(g.RequestFromCtx(ctx).URL.RawQuery)     // a=1&b=2
 	// fmt.Println(g.RequestFromCtx(ctx).URL.RequestURI()) // /testMeta?a=1&b=2
-
-	// fmt.Println(g.RequestFromCtx(ctx).Router)     // &{/testMeta GET default ^/testMeta$ [] 0}
-	// fmt.Println(g.RequestFromCtx(ctx).Router.Uri) // /testMeta
+	// fmt.Println(g.RequestFromCtx(ctx).Router)           // &{/testMeta GET default ^/testMeta$ [] 0}
+	// fmt.Println(g.RequestFromCtx(ctx).Router.Uri)       // /testMeta
 
 	// fmt.Println(grand.N(1000, 9999))
 	// fmt.Println(grand.Intn(1))
@@ -39,33 +66,8 @@ func (c *Test) Test(ctx context.Context, req *api.TestReq) (res *api.TestRes, er
 	// fmt.Println(grand.Letters(8)) //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 	// fmt.Println(grand.Symbols(8)) //!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
 
-	// g.Log().Info(ctx, `日志打印`, "\r\n", reqData)
-	// fmt.Println(g.I18n().T(ctx, `code.99999999`))
-
-	/* //合并Map
-	rawMap := gmap.NewStrAnyMapFrom(g.Map{`a`: 1, `b`: 2})
-	rawMap.Merge(gmap.NewStrAnyMapFrom(g.Map{`a`: 4, `c`: 3}))
-	fmt.Println(rawMap.Map()) */
-
-	// fmt.Println(garray.NewStrArrayFrom([]string{`a`, `b`, `c`}).Contains(`a`))	//是否含有元素
-
-	// fmt.Println(gset.NewIntSetFrom([]int{1, 2, 3}).Diff(gset.NewIntSetFrom([]int{1, 3})).Slice())	//差集
-
-	// g.Validator().Rules(`required|integer`).Data(`aaaa`).Run(ctx) //单独验证
-
-	// fmt.Println(genv.Set(`X_X`, `xx`))              //key必须由大写和_组成
-	// fmt.Println(g.Cfg().MustGetWithEnv(ctx, `X_X`)) //X_X或x_x或x.x方法都可以读取到
-
-	// fmt.Println(g.Cfg().MustGet(ctx, `superPlatformAdminId`).Uint())	//获取配置参数
-
-	// fmt.Println(g.DB(`default`).Model(`tab_user_unsubscribe`).Safe().Ctx(ctx))	//数据库连接
-
-	// fmt.Println(ghttp.RestartAllServer(ctx))	//重启服务
-
 	// g.RequestFromCtx(ctx).Response.Status = http.StatusMultipleChoices
-
 	// err = utils.NewErrorCode(ctx, 99999999, ``)
-
 	/* utils.HttpWriteJson(ctx, map[string]interface{}{
 		`info`: map[string]interface{}{},
 	}, 0, ``) */
