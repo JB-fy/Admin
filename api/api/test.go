cd67fd624845715209f8140ca54428a5d28297c2
JB <274456806@qq.com>
2023-12-04 09:49:41 +0800

    1

--- api/api/test.go
+++ api/api/test.go
@@ -7,7 +7,8 @@ import (
 type TestReq struct {
 	// g.Meta `path:"/test/*path" method:"get,post" mime:"application/json" deprecated:"废弃标记" tags:"测试。标签，用于分类" sm:"接口名称" dc:"详细描述"`
 	g.Meta `path:"/test" method:"get" mime:"application/json" deprecated:"true" tags:"测试。标签，用于分类" sm:"接口名称" dc:"详细描述"`
-	Test   string `json:"test" v:"required|length:4,30" in:"query" default:"默认值（嵌套结构体二级不起作用）" dc:"详细描述"`
+	//校验规则v在转换规则json之前执行，所以当结构体字段名与转换规则json中字段名不一致时，校验规则v中必须写别名，即此示例：test_test@，否则校验会报错
+	Test string `json:"test_test" v:"test_test@required|length:4,30#请输入测试字段|长度在4~30个字符之间" in:"query" default:"默认值（嵌套结构体二级不起作用）" dc:"测试字段"`
 }
 
 type TestRes struct {
