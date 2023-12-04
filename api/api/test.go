b409aa5828f8e60a1879441c919705efe97ca566
JB <274456806@qq.com>
2023-12-04 11:17:29 +0800

    1

--- api/api/test.go
+++ api/api/test.go
@@ -5,10 +5,11 @@ import (
 )
 
 type TestReq struct {
-	// g.Meta `path:"/test/*path" method:"get,post" mime:"application/json" deprecated:"废弃标记" tags:"测试。标签，用于分类" sm:"接口名称" dc:"详细描述"`
-	g.Meta `path:"/test" method:"get" mime:"application/json" deprecated:"true" tags:"测试。标签，用于分类" sm:"接口名称" dc:"详细描述"`
-	//校验规则v在转换规则json之前执行，所以当结构体字段名与转换规则json中字段名不一致时，校验规则v中必须写别名，即此示例：test_test@，否则校验会报错
-	Test string `json:"test_test" v:"test_test@required|length:4,30#请输入测试字段|长度在4~30个字符之间" in:"query" default:"默认值（嵌套结构体二级不起作用）" dc:"测试字段"`
+	// g.Meta `path:"/test/*path" method:"get,post" mime:"application/json" deprecated:"废弃标记" tags:"标签。用于分类" sm:"接口名称" dc:"详细描述"`
+	g.Meta `path:"/test" method:"get" deprecated:"true" tags:"测试" sm:"接口名称" dc:"详细描述"`
+	//当结构体字段名与参数传入字段名不一致时，验证规则必须设置属性别名，否则会默认以结构体字段名去寻找字段值做验证，形成隐患BUG
+	// Xxxx string `json:"xxxx,omitempty" v:"[属性别名@]校验规则1|校验规则2[#校验规则1错误提示|校验规则2错误提示]" in:"header/path/query/cookie" d:"默认值（嵌套结构体二级不起作用）" dc:"字段说明"`
+	Test string `json:"test_test,omitempty" v:"test_test@length:4,30#长度在4~30个字符之间" dc:"测试"`
 }
 
 type TestRes struct {
