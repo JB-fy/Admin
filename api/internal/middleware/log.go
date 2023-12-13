909862dfe3ed33ccfa77f71075216d8440bcc131
JB <274456806@qq.com>
2023-12-13 16:29:46 +0800

    1

--- api/internal/middleware/log.go
+++ api/internal/middleware/log.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"api/internal/utils"
+
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gtime"
@@ -20,6 +22,12 @@ func Log(r *ghttp.Request) {
 		`resData`:   r.Response.BufferString(),
 		`resStatus`: r.Response.Status,
 		`runTime`:   runTime,
+	}
+	data[`clientIp`] = r.GetClientIp()
+	data[`loginId`] = 0
+	loginInfo := utils.GetCtxLoginInfo(r.GetCtx())
+	if !loginInfo.IsEmpty() {
+		data[`loginId`] = loginInfo[`loginId`]
 	}
 	g.Log(`loggerHttp`).Info(r.GetCtx(), data)
 }
