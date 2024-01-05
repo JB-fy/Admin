5a693d155f2780891197ff8fe61a3c52e15e7d02
JB <274456806@qq.com>
2024-01-05 17:35:15 +0800

    1

--- view/platform/env.d.ts
+++ view/platform/env.d.ts
@@ -5,16 +5,4 @@ declare module '*.vue' {
     const componentOptions: ComponentOptions
     export default componentOptions
 }
-/* declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-    const component: DefineComponent<{}, {}, any>
-    export default component
-}
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
-interface ImportMetaEnv {
-    readonly VITE_HTTP_TIMEOUT: number
-} */
 declare module 'js-md5'
