4e639377dccdcf599d2122258e77f8fc1dbc4013
JB <274456806@qq.com>
2024-01-05 10:24:11 +0800

    1

--- view/platform/src/i18n/language/zh-cn/config.ts
+++ view/platform/src/i18n/language/zh-cn/config.ts
@@ -1,8 +1,8 @@
 // 当配置文件用（不用翻译。方便在HTML代码中使用，HTML不支持直接用import.meta.env.XXXX）
 export default {
     webTitle: '平台后台',
-    VITE_HTTP_API_PREFIX: import.meta.env.VITE_HTTP_API_PREFIX,
+    VITE_HTTP_API_PREFIX: import.meta.env.VITE_HTTP_API_PREFIX, //不用翻译
     const: {
-        tagType: ['', 'success', 'danger', 'info', 'warning']
+        tagType: ['', 'success', 'danger', 'info', 'warning'] //不用翻译
     }
 }
