d37e20d83e546d1c2e4a0c54257c746b94937348
JB <274456806@qq.com>
2024-01-06 14:10:21 +0800

    1

--- view/platform/src/main.ts
+++ view/platform/src/main.ts
@@ -14,11 +14,9 @@ app.use(createPinia())
 app.mount('#app')
 
 /*-------- 动态加载图标 开始 --------*/
-//app.component('autoiconEpLollipop', autoiconEpLollipop)
 import * as epIconList from '@element-plus/icons-vue'
 for (const [key, component] of Object.entries(epIconList)) {
-    //app.component('Ep' + key, component)
-    app.component('autoicon-ep-' + key, component) //兼容图标插件unplugin-icons，如插件以后支持动态加载<component :is="图标标识变量"/>，不用修改代码
+    app.component('AutoiconEp' + key, component) //兼容图标插件unplugin-icons，如插件以后支持动态加载<component :is="图标标识变量"/>，不用修改代码
 }
 /*-------- 动态加载图标 结束 --------*/
 
