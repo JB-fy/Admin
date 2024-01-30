8f2536ab66fb358d7c18915031316d75db9a929f
JB <274456806@qq.com>
2024-01-30 15:11:11 +0800

    1

--- api_py/main.py
+++ api_py/main.py
@@ -9,5 +9,8 @@ register_middleware(app)
 register_exception_handler(app)
 register_router(app)
 
+# 启动方式（两种）
+# 一般用于调试：uvicorn main:app --host=0.0.0.0 --port=8000 --reload
+# 线上服务器用：python3.12 main.py
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True, workers=1)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000)
