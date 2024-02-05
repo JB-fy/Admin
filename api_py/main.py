d5db871837f61f3ab63b21b5a7d83cd347e28411
JB <274456806@qq.com>
2024-02-05 17:22:32 +0800

    1

--- api_py/main.py
+++ api_py/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from middleware import register_middleware
 from exception import register_exception_handler
 from router import register_router
@@ -41,45 +41,19 @@ class TestRel(Base):
 from sqlalchemy.orm import Session
 
 
-""" def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def get_db(group: str = "default"):
+    async def func() -> Session:
+        try:
+            match group:
+                case "default":
+                    db = SessionLocal()
+                case _:
+                    db = SessionLocal()
+            yield db
+        finally:
+            db.close()
 
-def create_user(db: Session = Depends(get_db)):
-    db.query(Test).filter(Test.sceneId == 1).first() """
-
-
-def get_db(group: str = "default") -> Session:
-    match group:
-        case "default":
-            db = SessionLocal()
-        case "xxxx":
-            db = SessionLocal()
-        case _:
-            db = SessionLocal()
-    return db
-
-
-from sqlalchemy import func
-
-query = (
-    get_db().query(Test, Test.sceneName.label("label"), TestRel.relId, func.GROUP_CONCAT(TestRel.relId).label("relIdArr"))
-    # .join(TestRel, TestRel.sceneId == Test.sceneId, isouter=True)
-    # .outerjoin(TestRel, TestRel.sceneId == Test.sceneId)
-    # .options(load_only(Test.sceneId, Test.sceneName))
-    # .options(load_only(Test.isStop))
-    # .filter(Test.sceneId == 1)
-    # .group_by(Test.sceneId)
-    # .order_by(Test.sceneId.desc())
-    # .offset(0)
-    # .limit(1)
-)
-print(query)
-result = query.first()
-print(result)
+    return func
 
 
 def create_app():
@@ -87,9 +61,31 @@ def create_app():
     register_middleware(app)
     register_exception_handler(app)
     register_router(app)
-    """ @app.get("/test")
-    async def test():
-        return """
+
+    @app.get("/test")
+    async def test(db: Session = Depends(get_db())):
+        # from sqlalchemy import func
+        # db.query(Test, Test.sceneName.label("label"), TestRel.relId, func.GROUP_CONCAT(TestRel.relId).label("relIdArr"))
+        # .join(TestRel, TestRel.sceneId == Test.sceneId, isouter=True)
+        # .outerjoin(TestRel, TestRel.sceneId == Test.sceneId)
+        # .filter(Test.sceneId == 1)
+        # .group_by(Test.sceneId)
+        # .order_by(Test.sceneId.desc())
+        # .offset(0)
+        # .limit(1)
+
+        """async for db in get_db()():
+        query = db.query(Test)
+        print(query)
+        result = query.first()
+        print(result)"""
+
+        query = db.query(Test)
+        print(query)
+        result = query.first()
+        print(result)
+        return
+
     return app
 
 
