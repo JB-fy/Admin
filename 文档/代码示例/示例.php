de3ca38756a5c0a63a13cb1630ce0c3d9af697f7
JB <274456806@qq.com>
2024-03-15 09:31:31 +0800

    1

---
+++ 文档/代码示例/示例.php
@@ -0,0 +1,18 @@
+<?php
+
+/*--------ThinkPHP 开始--------*/
+// where('id', 'in', [])
+// where(['game_status' => 1, Db::raw('id=relation_game_id')])
+// where(['game_status'=>1, 'id'=>['exp', new Expression('=relation_game_id')]]);
+// where('id', 'exp', '=relation_game_id')
+// whereRaw('id = relation_game_id')
+// whereExp('id', '= relation_game_id')
+
+// update(['id' => xxxx])
+// insertGetId();
+// column('id');
+// value('id');
+
+// 打印sql
+// \think\Db::getLastSql()
+/*--------ThinkPHP 结束--------*/
