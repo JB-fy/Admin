620c5541a00f9b2aeb9b806ff32161c3dd0e6230
JB <274456806@qq.com>
2024-05-07 09:11:33 +0800

    1

--- api/internal/middleware/body_repeatable.go
+++ api/internal/middleware/body_repeatable.go
@@ -4,16 +4,18 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
-// GoFrame2.6版本POST表单请求(application/x-www-form-urlencoded 或 multipart/form-data)进入控制器后，r.GetBody()，r.GetBodyString()和r.Body都读取不到数据
-/* body, err := io.ReadAll(r.Body)
+/* // GoFrame2.6版本POST表单请求(application/x-www-form-urlencoded 或 multipart/form-data)进入控制器后，r.GetBody()，r.GetBodyString()和r.Body都读取不到数据
+body, err := io.ReadAll(r.Body)
 r.Body.Close()
 if err != nil {
 	return
 }
-bodyStr := string(body) */
+bodyStr := string(body)
 
 // 启用该中间件r.GetBody()和r.GetBodyString()一定能读取到数据
-// 设置repeatableRead为true，r.Body才能读取到数据
+// 设置repeatableRead为true，r.Body才能读取到数据 */
+
+// 2.7有改成和2.5版本一样。故可不用该中间件了
 func BodyRepeatable(repeatableRead bool) func(r *ghttp.Request) {
 	return func(r *ghttp.Request) {
 		r.MakeBodyRepeatableRead(repeatableRead)
--- api/internal/router/common.go
+++ api/internal/router/common.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"api/internal/controller"
-	"api/internal/middleware"
 
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gres"
@@ -21,36 +20,36 @@ func InitRouterCommon(s *ghttp.Server) {
 			// controllerThis.Sign,   //建议在场景内验证登录token后才可调用
 			// controllerThis.Config, //建议在场景内验证登录token后才可调用
 			controllerThis.Sts,
+			controllerThis.Notify,
 		)
-		group.Group(``, func(group *ghttp.RouterGroup) {
+		/* group.Group(``, func(group *ghttp.RouterGroup) {
 			group.Middleware(middleware.BodyRepeatable(true))
 			group.Bind(
 				controllerThis.Notify,
 			)
-		})
+		}) */
 	})
 	//支付
 	s.Group(`/pay`, func(group *ghttp.RouterGroup) {
 		controllerThis := controller.NewPay()
-		/* group.Bind(
+		group.Bind(
 			// controllerThis.List, //建议在场景内验证登录token后才可调用
 			// controllerThis.Pay, //建议在场景内验证登录token后才可调用
-		) */
-		group.Group(``, func(group *ghttp.RouterGroup) {
+			controllerThis.Notify,
+		)
+		/* group.Group(``, func(group *ghttp.RouterGroup) {
 			group.Middleware(middleware.BodyRepeatable(true))
 			group.Bind(
 				controllerThis.Notify,
 			)
-		})
+		}) */
 	})
 	//微信
 	s.Group(`/wx`, func(group *ghttp.RouterGroup) {
 		controllerThis := controller.NewWx()
-		group.Group(``, func(group *ghttp.RouterGroup) {
-			group.Bind(
-				controllerThis.GzhNotify,
-			)
-		})
+		group.Bind(
+			controllerThis.GzhNotify,
+		)
 	})
 	//测试
 	s.Group(``, func(group *ghttp.RouterGroup) {
