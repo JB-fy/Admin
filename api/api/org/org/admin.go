09a8e5d0d9a7bcfae758d458f28025c173687d37
JB <274456806@qq.com>
2024-06-17 17:59:32 +0800

    1

--- api/api/org/org/admin.go
+++ api/api/org/org/admin.go
@@ -17,10 +17,10 @@ type AdminInfo struct {
 	Email     *string     `json:"email,omitempty" dc:"邮箱"`
 	Account   *string     `json:"account,omitempty" dc:"账号"`
 	IsSuper   *uint       `json:"is_super,omitempty" dc:"超管：0否 1是"`
+	RoleIdArr []uint      `json:"role_id_arr,omitempty" dc:"角色ID列表"`
 	IsStop    *uint       `json:"is_stop,omitempty" dc:"停用：0否 1是"`
 	UpdatedAt *gtime.Time `json:"updated_at,omitempty" dc:"更新时间"`
 	CreatedAt *gtime.Time `json:"created_at,omitempty" dc:"创建时间"`
-	RoleIdArr []uint      `json:"role_id_arr,omitempty" dc:"角色ID列表"`
 	OrgName   *string     `json:"org_name,omitempty" dc:"机构"`
 }
 
@@ -38,13 +38,13 @@ type AdminFilter struct {
 	Phone          string      `json:"phone,omitempty" v:"max-length:20|phone" dc:"手机"`
 	Account        string      `json:"account,omitempty" v:"max-length:20|regex:^[\\p{L}][\\p{L}\\p{N}_]{3,}$" dc:"账号"`
 	IsSuper        *uint       `json:"is_super,omitempty" v:"in:0,1" dc:"超管：0否 1是"`
-	IsStop         *uint       `json:"is_stop,omitempty" v:"in:0,1" dc:"停用：0否 1是"`
 	RoleId         *uint       `json:"role_id,omitempty" v:"between:1,4294967295" dc:"角色ID"`
+	IsStop         *uint       `json:"is_stop,omitempty" v:"in:0,1" dc:"停用：0否 1是"`
 }
 
 /*--------列表 开始--------*/
 type AdminListReq struct {
-	g.Meta `path:"/admin/list" method:"post" tags:"机构后台/权限管理/机构管理员" sm:"列表"`
+	g.Meta `path:"/admin/list" method:"post" tags:"机构后台/权限管理/管理员" sm:"列表"`
 	Filter AdminFilter `json:"filter" dc:"过滤条件"`
 	Field  []string    `json:"field" v:"distinct|foreach|min-length:1" dc:"查询字段，传值参考返回的字段名，默认返回常用字段，如果所需字段较少或需特别字段时，可使用。特别注意：所需字段较少时使用，可大幅减轻数据库压力"`
 	Sort   string      `json:"sort" default:"id DESC" dc:"排序"`
@@ -61,7 +61,7 @@ type AdminListRes struct {
 
 /*--------详情 开始--------*/
 type AdminInfoReq struct {
-	g.Meta `path:"/admin/info" method:"post" tags:"机构后台/权限管理/机构管理员" sm:"详情"`
+	g.Meta `path:"/admin/info" method:"post" tags:"机构后台/权限管理/管理员" sm:"详情"`
 	Field  []string `json:"field" v:"distinct|foreach|min-length:1" dc:"查询字段，传值参考返回的字段名，默认返回常用字段，如果所需字段较少或需特别字段时，可使用。特别注意：所需字段较少时使用，可大幅减轻数据库压力"`
 	Id     uint     `json:"id" v:"required|between:1,4294967295" dc:"ID"`
 }
@@ -74,7 +74,7 @@ type AdminInfoRes struct {
 
 /*--------新增 开始--------*/
 type AdminCreateReq struct {
-	g.Meta    `path:"/admin/create" method:"post" tags:"机构后台/权限管理/机构管理员" sm:"新增"`
+	g.Meta    `path:"/admin/create" method:"post" tags:"机构后台/权限管理/管理员" sm:"新增"`
 	OrgId     *uint   `json:"org_id,omitempty" v:"between:0,4294967295" dc:"机构ID"`
 	Nickname  *string `json:"nickname,omitempty" v:"max-length:30" dc:"昵称"`
 	Avatar    *string `json:"avatar,omitempty" v:"max-length:200|url" dc:"头像"`
@@ -83,15 +83,15 @@ type AdminCreateReq struct {
 	Account   *string `json:"account,omitempty" v:"max-length:20|regex:^[\\p{L}][\\p{L}\\p{N}_]{3,}$" dc:"账号"`
 	Password  *string `json:"password,omitempty" v:"required|size:32" dc:"密码。md5保存"`
 	IsSuper   *uint   `json:"is_super,omitempty" v:"in:0,1" dc:"超管：0否 1是"`
+	RoleIdArr *[]uint `json:"role_id_arr,omitempty" v:"required|distinct|foreach|between:1,4294967295" dc:"角色ID列表"`
 	IsStop    *uint   `json:"is_stop,omitempty" v:"in:0,1" dc:"停用：0否 1是"`
-	RoleIdArr *[]uint `json:"role_id_arr,omitempty" v:"distinct|foreach|between:1,4294967295" dc:"角色ID列表"`
 }
 
 /*--------新增 结束--------*/
 
 /*--------修改 开始--------*/
 type AdminUpdateReq struct {
-	g.Meta    `path:"/admin/update" method:"post" tags:"机构后台/权限管理/机构管理员" sm:"修改"`
+	g.Meta    `path:"/admin/update" method:"post" tags:"机构后台/权限管理/管理员" sm:"修改"`
 	IdArr     []uint  `json:"id_arr,omitempty" v:"required|distinct|foreach|between:1,4294967295" dc:"ID数组"`
 	OrgId     *uint   `json:"org_id,omitempty" v:"between:0,4294967295" dc:"机构ID"`
 	Nickname  *string `json:"nickname,omitempty" v:"max-length:30" dc:"昵称"`
@@ -101,15 +101,15 @@ type AdminUpdateReq struct {
 	Account   *string `json:"account,omitempty" v:"max-length:20|regex:^[\\p{L}][\\p{L}\\p{N}_]{3,}$" dc:"账号"`
 	Password  *string `json:"password,omitempty" v:"size:32" dc:"密码。md5保存"`
 	IsSuper   *uint   `json:"is_super,omitempty" v:"in:0,1" dc:"超管：0否 1是"`
-	IsStop    *uint   `json:"is_stop,omitempty" v:"in:0,1" dc:"停用：0否 1是"`
 	RoleIdArr *[]uint `json:"role_id_arr,omitempty" v:"distinct|foreach|between:1,4294967295" dc:"角色ID列表"`
+	IsStop    *uint   `json:"is_stop,omitempty" v:"in:0,1" dc:"停用：0否 1是"`
 }
 
 /*--------修改 结束--------*/
 
 /*--------删除 开始--------*/
 type AdminDeleteReq struct {
-	g.Meta `path:"/admin/del" method:"post" tags:"机构后台/权限管理/机构管理员" sm:"删除"`
+	g.Meta `path:"/admin/del" method:"post" tags:"机构后台/权限管理/管理员" sm:"删除"`
 	IdArr  []uint `json:"id_arr,omitempty" v:"required|distinct|foreach|between:1,4294967295" dc:"ID数组"`
 }
 
