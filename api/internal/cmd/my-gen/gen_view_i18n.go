4edaf32deac099a8afab5113d75f73ff8821d5eb
JB <274456806@qq.com>
2024-07-05 12:00:06 +0800

    1

--- api/internal/cmd/my-gen/gen_view_i18n.go
+++ api/internal/cmd/my-gen/gen_view_i18n.go
@@ -151,8 +151,10 @@ func getViewI18nField(tpl myGenTpl, v myGenField) (viewI18nField myGenViewI18nFi
 			viewI18nField.name.DataTypeName = `'` + relIdObj.FieldName + `'`
 		}
 	case internal.TypeNameSortSuffix, internal.TypeNameNoSuffix: // sort,num,number,weight等后缀；	类型：int等类型；	// no,level,rank等后缀；	类型：int等类型；
-		viewI18nField.tip.Method = internal.ReturnTypeName
-		viewI18nField.tip.DataTypeName = `'` + v.FieldTip + `'`
+		if v.FieldTip != `` {
+			viewI18nField.tip.Method = internal.ReturnTypeName
+			viewI18nField.tip.DataTypeName = `'` + v.FieldTip + `'`
+		}
 	case internal.TypeNameStatusSuffix: // status,type,scene,method,pos,position,gender,currency等后缀；	类型：int等类型或varchar或char；	注释：多状态之间用[\s,，.。;；]等字符分隔。示例（状态：0待处理 1已处理 2驳回 yes是 no否）
 		viewI18nField.status.Method = internal.ReturnTypeName
 		if garray.NewIntArrayFrom([]int{internal.TypeVarchar, internal.TypeChar}).Contains(v.FieldType) {
