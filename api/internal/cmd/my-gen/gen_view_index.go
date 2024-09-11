73416017af11fa4098b1cc56b59f9e83b82ede3a
JB <274456806@qq.com>
2024-09-11 12:14:11 +0800

    1

--- api/internal/cmd/my-gen/gen_view_index.go
+++ api/internal/cmd/my-gen/gen_view_index.go
@@ -85,22 +85,22 @@ provide('saveCommon', saveCommon)`
 </script>
 
 <template>
-	<div v-if="!authAction.isRead" style="text-align: center; font-size: 60px; color: #f56c6c">{{ t('common.tip.notAuthActionRead') }}</div>
+    <div v-if="!authAction.isRead" style="text-align: center; font-size: 60px; color: #f56c6c">{{ t('common.tip.notAuthActionRead') }}</div>
     <template v-else>
-		<el-container class="main-table-container">
-			<el-header>
-				<query />
-			</el-header>
+        <el-container class="main-table-container">
+            <el-header>
+                <query />
+            </el-header>
 
-			<list :ref="(el: any) => listCommon.ref = el" />`
+            <list :ref="(el: any) => listCommon.ref = el" />`
 	if option.IsCreate || option.IsUpdate {
 		tplView += `
 
-			<!-- 加上v-if每次都重新生成组件。可防止不同操作之间的影响；新增操作数据的默认值也能写在save组件内 -->
-			<save v-if="saveCommon.visible" />`
+            <!-- 加上v-if每次都重新生成组件。可防止不同操作之间的影响；新增操作数据的默认值也能写在save组件内 -->
+            <save v-if="saveCommon.visible" />`
 	}
 	tplView += `
-		</el-container>
+        </el-container>
     </template>
 </template>
 `
