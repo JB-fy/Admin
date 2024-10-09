c9414e29b7328ddaf9e167f2e87cc6195c478f0e
JB <274456806@qq.com>
2024-10-09 21:36:48 +0800

    1

--- api/internal/controller/test.go
+++ api/internal/controller/test.go
@@ -94,9 +94,9 @@ func (c *Test) Test(ctx context.Context, req *api.TestReq) (res *api.TestRes, er
 	// g.RequestFromCtx(ctx).GetClientIp() // 192.168.0.44。常用于WEB编程，如HTTP
 	// g.RequestFromCtx(ctx).GetRemoteIp() // 192.168.0.44。常用于网络编程，如TCP或UDP
 
-	// g.RequestFromCtx(ctx).GetUrl()         // http://192.168.0.200:20080/testMeta?a=1&b=2
-	// g.RequestFromCtx(ctx).GetHost()        // 192.168.0.200
-	// g.RequestFromCtx(ctx).Host             // 192.168.0.200:20080
+	// g.RequestFromCtx(ctx).GetUrl()         // http://jb.admin.com:20080/testMeta?a=1&b=2
+	// g.RequestFromCtx(ctx).GetHost()        // jb.admin.com
+	// g.RequestFromCtx(ctx).Host             // jb.admin.com:20080
 	// g.RequestFromCtx(ctx).RequestURI       // /testMeta?a=1&b=2
 	// g.RequestFromCtx(ctx).URL.String()     // /testMeta?a=1&b=2
 	// g.RequestFromCtx(ctx).URL              // /testMeta?a=1&b=2
