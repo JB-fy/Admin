584ce388e3f2d6e6682931b55c4442a190003b7e
JB <274456806@qq.com>
2024-10-09 15:38:47 +0800

    1

--- api/api/test.go
+++ api/api/test.go
@@ -10,7 +10,7 @@ type TestReq struct {
 	// 当结构体字段名与参数传入字段名不一致时，验证规则必须设置属性别名，否则会默认以结构体字段名去寻找字段值做验证，形成隐患BUG
 	// Xxxx string `json:"xxxx,omitempty" v:"[属性别名@]校验规则1|校验规则2[#校验规则1错误提示|校验规则2错误提示]" in:"header/path/query/cookie" d:"默认值（嵌套结构体二级不起作用）" dc:"字段说明"`
 	Test string `json:"test,omitempty" v:"test@required|length:4,30#请输入测试字段|长度在4~30个字符之间" d:"test" dc:"测试"`
-	// Filter TestFilter `json:"filter" d:"{\"time_range_start\": \"2006-01-02 15:04:05\"}" dc:"过滤条件"`	//多层结构体嵌套很影响性能，并发减少很多
+	// Filter TestFilter `json:"filter" d:"{\"time_range_start\": \"2006-01-02 15:04:05\"}" dc:"过滤条件"`
 	/* Filter struct {
 		FilterTimeRangeStart *gtime.Time `json:"time_range_start,omitempty" v:"date-format:Y-m-d H:i:s" dc:"开始时间：YYYY-mm-dd HH:ii:ss"`
 	} `json:"where" dc:"查询条件"` */
