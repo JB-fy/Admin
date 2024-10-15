c8d8a1b8d34b7b6b62035ddac5745e1597b5bbb5
JB <274456806@qq.com>
2024-10-15 16:12:58 +0800

    1

--- api/internal/middleware/i18n.go
+++ api/internal/middleware/i18n.go
@@ -7,9 +7,6 @@ import (
 )
 
 func I18n(r *ghttp.Request) {
-	/* //建议放服务启动位置执行。省去重复执行
-	g.I18n().SetPath(g.Cfg().MustGet(r.GetCtx(), `i18n.path`).String())         //设置资源目录
-	g.I18n().SetLanguage(g.Cfg().MustGet(r.GetCtx(), `i18n.language`).String()) //设置默认为中文（原默认为英文en） */
 	language := r.Header.Get(`Language`)
 	if !(language == `` || language == g.Cfg().MustGet(r.GetCtx(), `i18n.language`).String()) {
 		ctx := gi18n.WithLanguage(r.GetCtx(), language)
