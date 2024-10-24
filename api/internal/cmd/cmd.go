49132ab61492dc64214b1d08b98d4445d644f04c
JB <274456806@qq.com>
2024-10-25 00:02:04 +0800

    1

--- api/internal/cmd/cmd.go
+++ api/internal/cmd/cmd.go
@@ -7,10 +7,13 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
+	"github.com/gogf/gf/v2/os/gfile"
 )
 
 var (
@@ -44,6 +47,32 @@ var (
 		Brief: `http服务`,
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
+
+			// 域名绑定：只建议在不想使用nginx且想要多域名绑定同一个端口时才使用
+			//	1、单域名绑定时，因域名访问还必须带上端口号，且必须做DNS解析到该服务器，所以用处不大
+			//	2、多域名绑定时，下方s.Xx等操作全得修改，且这样做还不如使用nginx做代理 或 直接使用两个端口 更方便
+			// d1 := s.Domain(`admin.xx.com`)
+			// d2 := s.Domain(`api.xx.com`)
+
+			// 开启静态文件服务时设置
+			serverRoot := g.Cfg().MustGet(ctx, `server.serverRoot`).String()
+			if serverRoot != `` {
+				// 上传文件目录设置
+				s.BindHookHandler(`/upload/*`, ghttp.HookBeforeServe, func(r *ghttp.Request) {
+					if r.IsFileRequest() {
+						// r.Response.CORSDefault()
+						r.Response.Header().Set(`Content-Disposition`, `attachment`) // 浏览器打开文件地址时，变为下载而不是显示
+					}
+				})
+				// 前端文件处理，无法做到nginx一样的效果：try_files $uri @backend;
+				s.BindHookHandler(`/admin/*`, ghttp.HookBeforeServe, func(r *ghttp.Request) {
+					pathArr := strings.Split(r.URL.Path, `/`)
+					path := `/` + pathArr[1] + `/` + pathArr[2]
+					if len(pathArr) > 3 && gfile.IsFile(serverRoot+path+`/index.html`) {
+						r.Response.RedirectTo(path)
+					}
+				})
+			}
 
 			s.BindMiddlewareDefault(middleware.Cross, middleware.I18n)
 			if g.Cfg().MustGet(ctx, `logger.http.isRecord`).Bool() {
