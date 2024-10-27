4987ec811cf39fea9113c497d53919fd5776e3fe
JB <274456806@qq.com>
2024-10-27 10:19:31 +0800

    1

--- api/internal/consts/consts.go
+++ api/internal/consts/consts.go
@@ -1,15 +1,15 @@
 package consts
 
 var (
-	CacheSaltFormat          = `salt_%s_%s`          //密码盐缓存key。参数：场景标识，手机/邮箱/账号/邮箱
-	CacheCodeFormat          = `code_%s_%s_%d`       //验证码缓存key。参数：场景标识，手机/邮箱，场景
-	CacheTokenActiveFormat   = `tokenActive_%s_%s`   //判断Token失活缓存key。参数：场景标识，登录用户ID
-	CacheTokenIsUniqueFormat = `tokenIsUnique_%s_%s` //判断Token唯一缓存key。参数：场景标识，登录用户ID
+	CacheSaltFormat          = `salt:%s:%s`          //密码盐缓存key。参数：场景标识，手机/邮箱/账号/邮箱
+	CacheCodeFormat          = `code:%s:%s_%d`       //验证码缓存key。参数：场景标识，手机/邮箱，场景
+	CacheTokenActiveFormat   = `tokenActive:%s:%s`   //判断Token失活缓存key。参数：场景标识，登录用户ID
+	CacheTokenIsUniqueFormat = `tokenIsUnique:%s:%s` //判断Token唯一缓存key。参数：场景标识，登录用户ID
 
 	ConstCtxSceneInfoName = `sceneInfo`
 	ConstCtxLoginInfoName = `loginInfo`
 
-	CacheWxGzhAccessToken = `wxGzhAccessToken_%s` //微信公众号授权Token缓存key。参数：微信公众号AppId
+	CacheWxGzhAccessToken = `wxGzhAccessToken:%s` //微信公众号授权Token缓存key。参数：微信公众号AppId
 )
 
 var (
