47a42abcf9e305dfa14979a9b1723e9f74bccbdc
JB <274456806@qq.com>
2024-11-29 10:24:19 +0800

    1

--- api/api/org/code.go
+++ api/api/org/code.go
@@ -6,7 +6,7 @@ import (
 
 /*--------发送验证码 开始--------*/
 type CodeSendReq struct {
-	g.Meta `path:"/send" method:"post" tags:"平台后台/验证码" sm:"发送"`
+	g.Meta `path:"/send" method:"post" tags:"机构后台/验证码" sm:"发送"`
 	Scene  uint   `json:"scene"  v:"required|in:4,14" dc:"场景：4绑定(手机) 14绑定(邮箱)"`
 	To     string `json:"to"  v:"required-if:Scene,4,Scene,14" dc:"手机/邮箱。scene=4,14时必须"`
 }
--- api/api/org/login.go
+++ api/api/org/login.go
@@ -6,7 +6,7 @@ import (
 
 /*--------获取密码盐 开始--------*/
 type LoginSaltReq struct {
-	g.Meta    `path:"/salt" method:"post" tags:"平台后台/登录" sm:"获取密码盐"`
+	g.Meta    `path:"/salt" method:"post" tags:"机构后台/登录" sm:"获取密码盐"`
 	OrgId     uint   `json:"org_id" v:"required|between:1,4294967295" dc:"机构ID"`
 	LoginName string `json:"login_name" v:"required|max-length:60" dc:"手机/邮箱/账号"`
 }
@@ -15,7 +15,7 @@ type LoginSaltReq struct {
 
 /*--------登录 开始--------*/
 type LoginLoginReq struct {
-	g.Meta    `path:"/login" method:"post" tags:"平台后台/登录" sm:"登录"`
+	g.Meta    `path:"/login" method:"post" tags:"机构后台/登录" sm:"登录"`
 	OrgId     uint   `json:"org_id" v:"required|between:1,4294967295" dc:"机构ID"`
 	LoginName string `json:"login_name" v:"required|max-length:60" dc:"手机/邮箱/账号"`
 	Password  string `json:"password" v:"required|size:32" dc:"密码。加密后发送，公式：md5(md5(md5(密码)+静态密码盐)+动态密码盐)"`
