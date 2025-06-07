dc9fc678bff02cc3e4cb26cd505c059db36d6de0
JB <274456806@qq.com>
2025-06-08 01:29:03 +0800

    1

--- view/platform/src/components/my-icon-dynamic.tsx
+++ view/platform/src/components/my-icon-dynamic.tsx
@@ -1,7 +1,7 @@
 import * as epIconList from '@element-plus/icons-vue'
 
 const iconMap: { [propName: string]: any } = {
-    // 'autoicon-mdi-account': <autoicon-mdi-account />,
+    // 'autoicon-material-symbols-person': <autoicon-material-symbols-person />,
 }
 for (const [key, component] of Object.entries(epIconList)) {
     iconMap['autoicon-ep-' + key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()] = component
