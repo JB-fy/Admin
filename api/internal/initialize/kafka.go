c58ef1e6856323702398027b985d6a66a54fd1fb
JB <274456806@qq.com>
2025-07-09 11:03:53 +0800

    1

--- api/internal/initialize/kafka.go
+++ api/internal/initialize/kafka.go
@@ -1,19 +1,24 @@
 package initialize
 
 import (
+	"api/internal/consts"
+	"api/internal/utils"
 	cluster_admin "api/internal/utils/kafka/cluster-admin"
 	"api/internal/utils/kafka/consumer"
 	"api/internal/utils/kafka/producer"
 	"context"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/genv"
 	"github.com/gogf/gf/v2/util/gconv"
 )
 
 func initKafka(ctx context.Context) {
 	for group, config := range g.Cfg().MustGet(ctx, `kafka`).Map() {
 		configMap := gconv.Map(config)
-		cluster_admin.Add(ctx, group, configMap)
+		if !utils.IsDev(ctx) && g.Cfg().MustGet(ctx, `masterServerNetworkIp`).String() != genv.Get(consts.ENV_SERVER_NETWORK_IP).String() {
+			cluster_admin.Add(ctx, group, configMap)
+		}
 		producer.Add(ctx, group, configMap)
 		consumer.Add(ctx, group, configMap)
 	}
