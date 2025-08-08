e89d858124e77536c606236a678c254f4e440f68
JB <274456806@qq.com>
2025-08-08 17:22:34 +0800

    1

--- api/internal/cache/internal/get_or_set.go
+++ api/internal/cache/internal/get_or_set.go
@@ -78,7 +78,7 @@ func (cacheThis *getOrSet) GetOrSet(ctx context.Context, key string, setFunc fun
 		}
 		if isSetVal.Bool() {
 			defer func() {
-				if rec := recover(); rec != nil { //防止panic导致消费者断开
+				if rec := recover(); rec != nil { //防止panic导致redis死锁
 					err = errors.New(`设置缓存panic错误：` + gconv.String(rec) + `。栈信息：` + string(debug.Stack()))
 					cacheThis.redis.Del(ctx, isSetKey) //报错时，删除redis锁缓存Key，允许其它服务器重新尝试设置缓存
 					g.Log().Error(ctx, err.Error())
