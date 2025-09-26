223f9dc1530a4271ec4d74726a1e6c6d8aa66973
JB <274456806@qq.com>
2025-09-26 11:10:36 +0800

    1

--- api/internal/utils/client.go
+++ api/internal/utils/client.go
@@ -5,19 +5,21 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/gclient"
-	"github.com/gogf/gf/v2/text/gstr"
 	"github.com/gogf/gf/v2/util/gconv"
 )
 
 var (
 	httpClient         = g.Client()
 	httpClientOfUpload = g.Client().Use(func(c *gclient.Client, r *http.Request) (resp *gclient.Response, err error) {
-		if gstr.Pos(r.URL.RawQuery, `formDataContentType=`) == 0 {
-			r.Header.Set(`Content-Type`, gstr.Replace(r.URL.RawQuery, `formDataContentType=`, ``, 1))
-			r.URL.RawQuery = ``
+		query := r.URL.Query()
+		if formDataContentType := query.Get(`formDataContentType`); formDataContentType != `` {
+			r.Header.Set(`Content-Type`, formDataContentType)
+			query.Del(`formDataContentType`)
+			r.URL.RawQuery = query.Encode()
 		}
 		resp, err = c.Next(r)
 		return
@@ -32,7 +34,7 @@ func HttpClientOfUpload() *gclient.Client {
 	return httpClientOfUpload
 }
 
-func PostFile(ctx context.Context, url string, param map[string]any, fileBytes []byte, fileFieldName string, fileName string) (res *gclient.Response, err error) {
+func PostFile(ctx context.Context, httpUrl string, param map[string]any, fileBytes []byte, fileFieldName string, fileName string) (res *gclient.Response, err error) {
 	buf := BytesBufferPoolGet()
 	defer BytesBufferPoolPut(buf)
 	writer := multipart.NewWriter(buf)
@@ -56,15 +58,19 @@ func PostFile(ctx context.Context, url string, param map[string]any, fileBytes [
 		return
 	}
 
-	// ContentType方法会从 ClientOfUpload复制一个新客户端，而每次上传只是Content-Type头不一样而已，没必要每次都复制一个新客户端
-	// res, err :=  ClientOfUpload.ContentType(writer.FormDataContentType()).Post(ctx, url, buf)
-	// 将writer.FormDataContentType()当作query传递，在 Client的拦截器中识别并设置Content-Type头。减少频繁复制 ClientOfUpload客户端
-	res, err = HttpClientOfUpload().Post(ctx, url+`?formDataContentType=`+writer.FormDataContentType(), buf)
+	// ContentType方法会从ClientOfUpload复制一个新客户端，而每次上传只是Content-Type头不一样而已，没必要每次都复制一个新客户端
+	// res, err := ClientOfUpload.ContentType(writer.FormDataContentType()).Post(ctx, httpUrl, buf)
+	// 将writer.FormDataContentType()当作query传递，在拦截器中识别并设置Content-Type头。减少频繁复制ClientOfUpload客户端
+	parseUrl, _ := url.Parse(httpUrl)
+	parseUrlQuery := parseUrl.Query()
+	parseUrlQuery.Set(`formDataContentType`, writer.FormDataContentType())
+	parseUrl.RawQuery = parseUrlQuery.Encode()
+	res, err = HttpClientOfUpload().Post(ctx, parseUrl.String(), buf)
 	return
 }
 
-func IsExistFile(ctx context.Context, url string) (isExist bool, err error) {
-	res, err := HttpClient().Head(ctx, url)
+func IsExistFile(ctx context.Context, httpUrl string) (isExist bool, err error) {
+	res, err := HttpClient().Head(ctx, httpUrl)
 	if err != nil {
 		return
 	}
