2bd9bc6611423035714ed8ac128a80d6752a6064
JB <274456806@qq.com>
2025-10-26 17:48:07 +0800

    1

---
+++ api/internal/cache/get-or-set/get_or_set_redis.go
@@ -0,0 +1,36 @@
+package get_or_set
+
+import (
+	"api/internal/utils/jbredis"
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var GetOrSetRedis = getOrSetRedis{}
+
+type getOrSetRedis struct{}
+
+func (cacheThis *getOrSetRedis) cache() redis.UniversalClient {
+	return jbredis.DB()
+}
+
+func (cacheThis *getOrSetRedis) GetOrSet(ctx context.Context, key string, setFunc func() (value any, notExist bool, err error), ttl time.Duration, numLock, numRead uint8, oneTime time.Duration) (value any, notExist bool, err error) {
+	value, notExist, err = GetOrSet.GetOrSet(ctx, key, func() (value any, notExist bool, err error) {
+		value, notExist, err = setFunc()
+		if notExist || err != nil {
+			return
+		}
+		err = cacheThis.cache().SetEx(ctx, key, value, ttl).Err()
+		return
+	}, func() (value any, notExist bool, err error) {
+		value, err = cacheThis.cache().Get(ctx, key).Result()
+		notExist = err == redis.Nil
+		if notExist {
+			err = nil
+		}
+		return
+	}, 0, 0, 0)
+	return
+}
