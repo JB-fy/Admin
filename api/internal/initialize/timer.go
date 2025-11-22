4c40f84a32b271038c7724ba1d7dfe95f316e6ef
JB <274456806@qq.com>
2025-11-22 17:50:43 +0800

    1

--- api/internal/initialize/timer.go
+++ api/internal/initialize/timer.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/genv"
-	"github.com/gogf/gf/v2/os/gtimer"
 )
 
 func initTimer(ctx context.Context) {
@@ -18,25 +17,31 @@ func initTimer(ctx context.Context) {
 		return
 	}
 
-	// myTimerThis := myTimer{}
-	// myTimerThis.CACHE_WX_GZH_ACCESS_TOKEN(ctx) //缓存微信公众号AccessToken（需要时启用，且公众号需设置IP白名单）
+	myTimerThis := myTimer{}
+
+	myTimerThis.CACHE_WX_GZH_ACCESS_TOKEN(ctx) //缓存微信公众号AccessToken（需要时启用，且公众号需设置IP白名单）
+	// gtimer.SetTimeout(ctx, 5*time.Second, myTimerThis.CACHE_WX_GZH_ACCESS_TOKEN)
 }
 
 type myTimer struct{}
 
 func (myTimerThis *myTimer) CACHE_WX_GZH_ACCESS_TOKEN(ctx context.Context) {
-	wxGzhObj := wx.NewWxGzh(ctx)
-	accessTokenInfo, err := wxGzhObj.AccessToken(ctx)
-	if err != nil {
-		g.Log().Error(ctx, `获取微信公众号AccessToken接口错误：`+err.Error(), err)
-		gtimer.SetTimeout(ctx, 5*time.Second, myTimerThis.CACHE_WX_GZH_ACCESS_TOKEN) //5秒后重试
-		return
-	}
-	err = cache.WxGzhAccessToken.Set(ctx, wxGzhObj.AppId, accessTokenInfo.AccessToken, time.Duration(accessTokenInfo.ExpiresIn)*time.Second)
-	if err != nil {
-		g.Log().Error(ctx, `缓存微信公众号AccessToken错误：`+err.Error(), err)
-		gtimer.SetTimeout(ctx, 5*time.Second, myTimerThis.CACHE_WX_GZH_ACCESS_TOKEN) //5秒后重试
-		return
-	}
-	gtimer.SetTimeout(ctx, time.Duration(accessTokenInfo.ExpiresIn-30)*time.Second, myTimerThis.CACHE_WX_GZH_ACCESS_TOKEN) //提早30秒刷新缓存
+	go func() {
+		for {
+			wxGzhObj := wx.NewWxGzh(ctx)
+			accessTokenInfo, err := wxGzhObj.AccessToken(ctx)
+			if err != nil {
+				g.Log().Error(ctx, `获取微信公众号AccessToken接口错误：`+err.Error(), err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			err = cache.WxGzhAccessToken.Set(ctx, wxGzhObj.AppId, accessTokenInfo.AccessToken, time.Duration(accessTokenInfo.ExpiresIn)*time.Second)
+			if err != nil {
+				g.Log().Error(ctx, `缓存微信公众号AccessToken错误：`+err.Error(), err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			time.Sleep(time.Duration(accessTokenInfo.ExpiresIn-30) * time.Second) //提早30秒刷新缓存
+		}
+	}()
 }
