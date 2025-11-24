c4c5bd1099d1f720ce9d17e6f72ef7fa7dbf6fc4
JB <274456806@qq.com>
2025-11-24 13:43:27 +0800

    1

--- view/platform/src/components/MyExportButton.vue
+++ view/platform/src/components/MyExportButton.vue
@@ -3,7 +3,7 @@
     :api="{ code: t('config.VITE_HTTP_API_PREFIX') + '/auth/test/list', param: { filter: queryCommon.data, sort: table.sort.key + ' ' + table.sort.order } }" />
 
 <my-export-button fileName="文件名.xlsx" i18nPrefix="auth.test" :headerList="table.columns"
-    :api="{ code: t('config.VITE_HTTP_API_PREFIX') + '/auth/test/list', param: { filter: queryCommon.data, sort: table.sort.key + ' ' + table.sort.order }, limit: 0 }" /> -->
+    :api="{ code: t('config.VITE_HTTP_API_PREFIX') + '/auth/test/list', param: { filter: queryCommon.data, sort: table.sort.key + ' ' + table.sort.order, limit: 0 } }" /> -->
 <!-------- 使用示例 结束-------->
 <script setup lang="tsx">
 const { t, tm } = useI18n()
