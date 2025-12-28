760127b78edcd3109fc523b56285cfe9a29c2740
JB <274456806@qq.com>
2025-12-28 14:25:18 +0800

    1

--- api/api/common.go
+++ api/api/common.go
@@ -37,6 +37,11 @@ type CommonListReq struct {
 	Limit int    `json:"limit" v:"min:0" default:"10" dc:"每页数量。可传0取全部"`
 }
 
+type CommonSignReq struct {
+	Ts   uint   `json:"ts" v:"" dc:"时间戳。单位：秒"`
+	Sign string `json:"sign" v:"" dc:"签名。所有参数按ASCII码排序后拼接（值是对象数组等复杂数据时，要转json格式拼接），再拼接密钥key，最后md5得到签名。公式：md5(参数1=值&参数2=值&....&key=密钥)"`
+}
+
 type CommonNoDataRes struct{}
 
 type CommonCreateRes struct {
