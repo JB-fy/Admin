87e58e374b9b268ff6caed522b5d91a5ce1dfd46
JB <274456806@qq.com>
2026-02-04 14:20:44 +0800

    1

--- api/internal/cmd/my-gen/gen_router.go
+++ api/internal/cmd/my-gen/gen_router.go
@@ -14,24 +14,25 @@ func genRouter(option myGenOption, tpl *myGenTpl) {
 
 	moduleName := tpl.GetModuleName(`controller`)
 	importControllerStr := `"api/internal/controller/` + option.SceneId + `/` + tpl.ModuleDirCaseKebab + `"`
-	if !strings.Contains(tplRouter, importControllerStr) {
+	if strings.Contains(tplRouter, importControllerStr) {
+		if strings.Contains(tplRouter, `group.Bind(`+moduleName+`.New`+tpl.TableCaseCamel+`())`) {
+			return
+		}
+		// 路由生成（controller已导入，但路由不存在）
+		replacePoint := `group.Group(` + "`" + `/` + tpl.ModuleDirCaseKebab + "`" + `, func(group *ghttp.RouterGroup) {`
+		replaceIndex := strings.LastIndex(tplRouter, replacePoint)
+		tplRouter = tplRouter[:replaceIndex] + replacePoint + `
+				group.Bind(` + moduleName + `.New` + tpl.TableCaseCamel + `())` + tplRouter[replaceIndex+len(replacePoint):]
+		utils.FilePutFormat(saveFile, []byte(tplRouter)...)
+	} else { // 路由生成（controller未导入）
 		tplRouter = strings.Replace(tplRouter, `"api/internal/middleware"`, importControllerStr+`
 	"api/internal/middleware"`, 1)
-		// 路由生成（controller未导入）
+
 		tplRouter = strings.Replace(tplRouter, `/*--------后端路由自动代码生成锚点（不允许修改和删除，否则将不能自动生成路由）--------*/`, `group.Group(`+"`"+`/`+tpl.ModuleDirCaseKebab+"`"+`, func(group *ghttp.RouterGroup) {
 				group.Bind(`+moduleName+`.New`+tpl.TableCaseCamel+`())
 			})
 
 			/*--------后端路由自动代码生成锚点（不允许修改和删除，否则将不能自动生成路由）--------*/`, 1)
 		utils.FilePutFormat(saveFile, []byte(tplRouter)...)
-	} else {
-		// 路由生成（controller已导入，但路由不存在）
-		if !strings.Contains(tplRouter, `group.Bind(`+moduleName+`.New`+tpl.TableCaseCamel+`())`) {
-			replacePoint := `group.Group(` + "`" + `/` + tpl.ModuleDirCaseKebab + "`" + `, func(group *ghttp.RouterGroup) {`
-			idx := strings.LastIndex(tplRouter, replacePoint)
-			tplRouter = tplRouter[:idx] + replacePoint + `
-				group.Bind(` + moduleName + `.New` + tpl.TableCaseCamel + `())` + tplRouter[idx+len(replacePoint):]
-			utils.FilePutFormat(saveFile, []byte(tplRouter)...)
-		}
 	}
 }
