1dac25f233eda7dd36c9d88ea0964b691e5fb0bb
JB <274456806@qq.com>
2026-05-05 11:54:38 +0800

    1

--- api/internal/cmd/my-gen/tpl.go
+++ api/internal/cmd/my-gen/tpl.go
@@ -1189,14 +1189,16 @@ func (myGenTplThis *myGenTpl) getOtherRel(ctx context.Context) {
 		return
 	}
 	if !slices.Contains([]string{`id`, myGenTplThis.TableCaseSnake + `_id`}, myGenTplThis.Handle.Id.List[0].FieldCaseSnake) { //可能是扩展表
-		fmt.Println(color.HiYellowString(`该表主键命名不符合规范，可能是其它表的扩展表，是则无需查找其它关联表，需手动确认`))
+		fmt.Println(color.HiYellowString(`该表主键命名不符合规范，是否继续查找其它关联表（比如是其它表的扩展表时，则无需继续查找），需手动确认`))
 		isContinueStr := gcmd.Scan(color.BlueString(`> 是否继续查找其它关联表，请确认？默认(no)：`))
 	isContinueEnd:
 		for {
 			switch isContinueStr {
 			case `1`, `yes`:
+				myGenTplThis.Handle.OtherRelTableCmdLog = append(myGenTplThis.Handle.OtherRelTableCmdLog, fmt.Sprintf(`%s:%s`, `继续查找其它关联表`, `是`))
 				break isContinueEnd
 			case ``, `0`, `no`:
+				myGenTplThis.Handle.OtherRelTableCmdLog = append(myGenTplThis.Handle.OtherRelTableCmdLog, fmt.Sprintf(`%s:%s`, `继续查找其它关联表`, `否`))
 				return
 			default:
 				isContinueStr = gcmd.Scan(color.RedString(`    输入错误，请重新输入，是否继续查找其它关联表，请确认？默认(no)：`))
