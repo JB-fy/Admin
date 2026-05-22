40e18f484c1b5238090c2dab6541a1a8bff61c57
JB <274456806@qq.com>
2026-05-22 10:18:29 +0800

    1

--- api/internal/utils/img.go
+++ api/internal/utils/img.go
@@ -31,12 +31,22 @@ type ImgOption struct {
 	Width           int            `json:"width"`
 	Height          int            `json:"height"`
 	MaxSize         int            `json:"max_size"`          //最大字节数
+	Quality         int            `json:"quality"`           //JPEG压缩数值。范围1~100，大于0生效
+	Sigma           float64        `json:"sigma"`             //非JPEG压缩数值。高斯模糊，大于0生效
 	RatioArr        []string       `json:"ratio_arr"`         //宽高比：1:1, 3:4。比例正确时，不修改宽高
 	EncodeFormatArr []string       `json:"encode_format_arr"` //需要转换的格式：video/jpeg, image/webp
 	TargetFormat    imaging.Format `json:"target_format"`     //当EncodeFormatArr不为空，且需要格式转换时才有用，用于指定转换后的目标格式，默认：imaging.JPEG
-	IsErrorBefore   bool           `json:"is_error_before"`   //处理前不符合就报错：0否 1是
-	IsErrorAfter    bool           `json:"is_error_after"`    //处理后不符合才报错：0否 1是
+	IsError         bool           `json:"is_error"`          //报错：0否 1是
 }
+
+var (
+	ImgQualityInit  = 95
+	ImgQualityStep  = 10
+	ImgQualityLimit = 50
+	ImgSigmaInit    = 0.0
+	ImgSigmaStep    = 0.5
+	ImgSigmaLimit   = 2.0
+)
 
 func ImgHandle(imgBytesOfRaw []byte, imgOption ImgOption) (imgBytes []byte, err error) {
 	imgBytes = imgBytesOfRaw
@@ -47,12 +57,12 @@ func ImgHandle(imgBytesOfRaw []byte, imgOption ImgOption) (imgBytes []byte, err 
 	}
 	isHandle := len(imgOption.EncodeFormatArr) > 0 && slices.Contains(imgOption.EncodeFormatArr, imgType)
 	if isHandle {
-		if imgOption.IsErrorBefore {
+		if imgOption.IsError {
 			err = fmt.Errorf(`图片格式不支持：%s`, imgType)
 			return
 		}
 		format = imgOption.TargetFormat
-	} else if (imgOption.Width == 0 || imgOption.Height == 0) && imgOption.MaxSize == 0 {
+	} else if (imgOption.Width == 0 || imgOption.Height == 0) && imgOption.MaxSize == 0 && ((format == imaging.JPEG && imgOption.Quality == 0) || (format != imaging.JPEG && imgOption.Sigma == 0)) {
 		return
 	}
 	imgObj, err := ImgDecode(imgBytes)
@@ -62,14 +72,14 @@ func ImgHandle(imgBytesOfRaw []byte, imgOption ImgOption) (imgBytes []byte, err 
 	if imgOption.Width > 0 && imgOption.Height > 0 && !(imgObj.Bounds().Dx() == imgOption.Width && imgObj.Bounds().Dy() == imgOption.Height) {
 		if len(imgOption.RatioArr) == 0 {
 			if !(imgObj.Bounds().Dx() == imgOption.Width && imgObj.Bounds().Dy() == imgOption.Height) {
-				if imgOption.IsErrorBefore {
+				if imgOption.IsError {
 					err = fmt.Errorf(`图片宽高不符合要求：宽%d,高%d`, imgOption.Width, imgOption.Height)
 					return
 				}
 				isHandle = true
 			}
 		} else if !slices.Contains(imgOption.RatioArr, GetRatio(imgObj.Bounds().Dx(), imgObj.Bounds().Dy())) {
-			if imgOption.IsErrorBefore {
+			if imgOption.IsError {
 				err = fmt.Errorf(`图片宽高比不符合要求：%s`, gconv.String(imgOption.RatioArr))
 				return
 			}
@@ -79,38 +89,51 @@ func ImgHandle(imgBytesOfRaw []byte, imgOption ImgOption) (imgBytes []byte, err 
 			imgObj = imaging.Fill(imgObj, imgOption.Width, imgOption.Height, imaging.Center, imaging.NearestNeighbor)
 		}
 	}
+	quality := ImgQualityInit
+	sigma := ImgSigmaInit
+	var opts []imaging.EncodeOption
+	switch format {
+	case imaging.JPEG:
+		if imgOption.Quality > 0 {
+			quality = imgOption.Quality
+			opts = []imaging.EncodeOption{imaging.JPEGQuality(quality)}
+			isHandle = true
+		}
+	// case imaging.PNG:
+	// 	opts = []imaging.EncodeOption{imaging.PNGCompressionLevel(png.BestCompression)} //实测大小没变化
+	// case imaging.GIF:
+	// case imaging.TIFF:
+	// case imaging.BMP:
+	default: //统一使用：高斯模糊
+		if imgOption.Sigma > 0 {
+			sigma = imgOption.Sigma
+			imgObj = imaging.Blur(imgObj, sigma)
+			isHandle = true
+		}
+	}
 	if isHandle {
 		// buf.Bytes()返回的字节后续还要使用。无法用连接池
 		// buf := BytesBufferPoolGet()
 		// defer BytesBufferPoolPut(buf)
 		buf := bytes.NewBuffer(nil)
-		err = imaging.Encode(buf, imgObj, format)
+		err = imaging.Encode(buf, imgObj, format, opts...)
 		if err != nil {
 			return
 		}
 		imgBytes = buf.Bytes()
 	}
 	if imgOption.MaxSize > 0 && len(imgBytes) > imgOption.MaxSize {
-		quality := 95
-		qualityStep := 10
-		qualityLimit := 50
-		sigma := 0.0
-		sigmaStep := 0.5
-		sigmaLimit := 2.0
-		var opts []imaging.EncodeOption
 		buf := bytes.NewBuffer(nil)
 		for len(imgBytes) > imgOption.MaxSize {
-			if imgOption.IsErrorBefore {
+			if imgOption.IsError {
 				err = fmt.Errorf(`图片大小不符合要求：最大%d`, imgOption.MaxSize)
 				return
 			}
 			switch format {
 			case imaging.JPEG:
-				quality -= qualityStep
-				if quality < qualityLimit {
-					if imgOption.IsErrorAfter {
-						err = fmt.Errorf(`图片压缩后大小不符合要求：最大%d,图片质量%d。继续压缩无意义，会变得模糊`, imgOption.MaxSize, quality)
-					}
+				quality -= ImgQualityStep
+				if quality < ImgQualityLimit {
+					err = fmt.Errorf(`图片压缩后大小不符合要求：最大%d,图片质量%d。继续压缩无意义，会变得模糊`, imgOption.MaxSize, quality)
 					return
 				}
 				opts = []imaging.EncodeOption{imaging.JPEGQuality(quality)}
@@ -120,11 +143,9 @@ func ImgHandle(imgBytesOfRaw []byte, imgOption ImgOption) (imgBytes []byte, err 
 			// case imaging.TIFF:
 			// case imaging.BMP:
 			default: //统一使用：高斯模糊
-				sigma += sigmaStep
-				if sigma > sigmaLimit {
-					if imgOption.IsErrorAfter {
-						err = fmt.Errorf(`图片压缩后大小不符合要求：最大%d,高斯模糊%f。继续压缩无意义，会变得模糊`, imgOption.MaxSize, sigma)
-					}
+				sigma += ImgSigmaStep
+				if sigma > ImgSigmaLimit {
+					err = fmt.Errorf(`图片压缩后大小不符合要求：最大%d,高斯模糊%f。继续压缩无意义，会变得模糊`, imgOption.MaxSize, sigma)
 					return
 				}
 				imgObj = imaging.Blur(imgObj, sigma)
