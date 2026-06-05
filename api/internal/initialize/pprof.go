0c82f2eb023e2185f75f56a35666b9850af5df4b
JB <274456806@qq.com>
2026-06-05 17:00:29 +0800

    1

--- api/internal/initialize/pprof.go
+++ api/internal/initialize/pprof.go
@@ -7,6 +7,19 @@ import (
 )
 
 func initPprof(ctx context.Context) {
+	/* gtimer.SetInterval(ctx, 5*time.Second, func(ctx context.Context) {
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+		if m.HeapInuse > 4*1024*1024*1024 { //内存超过多少时，记录pprof
+			buf := utils.BytesBufferPoolGet()
+			defer utils.BytesBufferPoolPut(buf)
+			err := pprof.WriteHeapProfile(buf)
+			if err != nil {
+				return
+			}
+			gfile.PutBytes(gfile.SelfDir()+`/log/pprof/heap.pprof`, buf.Bytes())
+		}
+	}) */
 	go func() {
 		http.ListenAndServe(`:6060`, nil)
 	}()
